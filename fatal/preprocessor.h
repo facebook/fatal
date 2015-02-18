3d4f862ee41792e5e2aa3137623e4ffb8c2ba764
Marcelo Juchem <marcelo@fb.com>
2015-02-18 15:16:42 -0800

    - reorganizing preprocessor.h - implementing more unit tests for preprocessor.h - adding FATAL_IS_PARENTHESIZED and FATAL_UNPARENTHESIZE - fixing {FATA_HAS_ARGS|FATAL_MAP|FATAL_SIMPLE_MAP} for situations where   first argument is enclosed in parenthesis

--- fatal/preprocessor.h
+++ fatal/preprocessor.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2014, Facebook, Inc.
+ *  Copyright (c) 2015, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -16,185 +16,354 @@ namespace fatal {
 // FATAL_EMPTY //
 /////////////////
 
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 #define FATAL_EMPTY()
 
 //////////////////
 // FATAL_IGNORE //
 //////////////////
 
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 #define FATAL_IGNORE(...)
 
 ////////////////////
 // FATAL_IDENTITY //
 ////////////////////
 
-#define FATAL_IDENTITY(...) __VA_ARGS__
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_IDENTITY(...) \
+  __VA_ARGS__
 
 ///////////////
 // FATAL_CAT //
 ///////////////
 
-#define FATAL_CAT(LHS, RHS) FATAL_CAT_IMPL(LHS, RHS)
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_CAT(LHS, ...) \
+  FATAL_IMPL_CAT(LHS, __VA_ARGS__)
+
+#define FATAL_IMPL_CAT(LHS, ...) \
+  LHS ## __VA_ARGS__
 
 ///////////////
 // FATAL_NOT //
 ///////////////
 
-#define FATAL_NOT(x) \
-  FATAL_NOT_IMPL(FATAL_CAT(FATAL_NOT_IMPL_, x))
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_NOT(...) \
+  FATAL_IMPL_NOT(FATAL_CAT(FATAL_IMPL_NOT_SWITCH_, __VA_ARGS__)())
+
+#define FATAL_IMPL_NOT(...) \
+  FATAL_ARG_2(__VA_ARGS__, 0)
+
+#define FATAL_IMPL_NOT_SWITCH_() \
+  FATAL_IMPL_NOT_WHEN_FALSE()
+
+#define FATAL_IMPL_NOT_SWITCH_0(...) \
+  FATAL_IMPL_NOT_WHEN_FALSE()
+
+#define FATAL_IMPL_NOT_WHEN_FALSE() \
+  ~, 1
 
 ////////////////
 // FATAL_BOOL //
 ////////////////
 
-#define FATAL_BOOL(x) FATAL_NOT(FATAL_NOT(x))
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_BOOL(...) \
+  FATAL_NOT(FATAL_NOT(__VA_ARGS__))
+
+////////////////////////////
+// FATAL_IS_PARENTHESIZED //
+////////////////////////////
+
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_IS_PARENTHESIZED(...) \
+  FATAL_IMPL_IS_PARENTHESIZED(FATAL_IMPL_IS_PARENTHESIZED_WHEN_TRUE __VA_ARGS__)
+
+#define FATAL_IMPL_IS_PARENTHESIZED(...) \
+  FATAL_NOT(FATAL_ARG_1(__VA_ARGS__))
+
+#define FATAL_IMPL_IS_PARENTHESIZED_WHEN_TRUE(...) \
+  0
+
+//////////////////////////
+// FATAL_UNPARENTHESIZE //
+//////////////////////////
+
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_UNPARENTHESIZE(...) \
+  FATAL_CONDITIONAL(FATAL_IS_PARENTHESIZED(__VA_ARGS__))( \
+    FATAL_IDENTITY __VA_ARGS__ \
+  )( \
+    __VA_ARGS__ \
+  )
 
 /////////////////
 // FATAL_ARG_n //
 /////////////////
 
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 #define FATAL_ARG_1(x1, ...) x1
+
 #define FATAL_ARG_2(x1, x2, ...) x2
+
 #define FATAL_ARG_3(x1, x2, x3, ...) x3
+
 #define FATAL_ARG_4(x1, x2, x3, x4, ...) x4
+
 #define FATAL_ARG_5(x1, x2, x3, x4, x5, ...) x5
+
+#define FATAL_ARG_6(x1, x2, x3, x4, x5, x6, ...) x6
+
+#define FATAL_ARG_7(x1, x2, x3, x4, x5, x6, x7, ...) x7
+
+#define FATAL_ARG_8(x1, x2, x3, x4, x5, x6, x7, x8, ...) x8
+
+#define FATAL_ARG_9(x1, x2, x3, x4, x5, x6, x7, x8, x9, ...) x9
+
+#define FATAL_ARG_10(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, ...) x10
+
+#define FATAL_ARG_11(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, ...) x11
+
+#define FATAL_ARG_12( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, ... \
+) x12
+
+#define FATAL_ARG_13( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, ... \
+) x13
+
+#define FATAL_ARG_14( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, ... \
+) x14
+
+#define FATAL_ARG_15( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, ... \
+) x15
+
+#define FATAL_ARG_16( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, ... \
+) x16
+
+///////////////////////
+// FATAL_SKIP_n_ARGS //
+///////////////////////
+
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_SKIP_1_ARG(x1, ...) __VA_ARGS__
+
+#define FATAL_SKIP_2_ARGS(x1, x2, ...) __VA_ARGS__
+
+#define FATAL_SKIP_3_ARGS(x1, x2, x3, ...) __VA_ARGS__
+
+#define FATAL_SKIP_4_ARGS(x1, x2, x3, x4, ...) __VA_ARGS__
+
+#define FATAL_SKIP_5_ARGS(x1, x2, x3, x4, x5, ...) __VA_ARGS__
+
+#define FATAL_SKIP_6_ARGS(x1, x2, x3, x4, x5, x6, ...) __VA_ARGS__
+
+#define FATAL_SKIP_7_ARGS(x1, x2, x3, x4, x5, x6, x7, ...) __VA_ARGS__
+
+#define FATAL_SKIP_8_ARGS(x1, x2, x3, x4, x5, x6, x7, x8, ...) __VA_ARGS__
+
+#define FATAL_SKIP_9_ARGS(x1, x2, x3, x4, x5, x6, x7, x8, x9, ...) __VA_ARGS__
+
+#define FATAL_SKIP_10_ARGS( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, ... \
+) __VA_ARGS__
+
+#define FATAL_SKIP_11_ARGS( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, ... \
+) __VA_ARGS__
+
+#define FATAL_SKIP_12_ARGS( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, ... \
+) __VA_ARGS__
+
+#define FATAL_SKIP_13_ARGS( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, ... \
+) __VA_ARGS__
+
+#define FATAL_SKIP_14_ARGS( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, ... \
+) __VA_ARGS__
+
+#define FATAL_SKIP_15_ARGS( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, ... \
+) __VA_ARGS__
+
+#define FATAL_SKIP_16_ARGS( \
+  x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, ... \
+) __VA_ARGS__
 
 ////////////////////
 // FATAL_HAS_ARGS //
 ////////////////////
 
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 #define FATAL_HAS_ARGS(...) \
-  FATAL_BOOL(FATAL_ARG_1(FATAL_HAS_ARGS_IMPL __VA_ARGS__)())
+  FATAL_BOOL( \
+    FATAL_ARG_1( \
+      FATAL_IMPL_HAS_ARGS_1( \
+        FATAL_UNPARENTHESIZE(__VA_ARGS__) \
+      ) \
+    )() \
+  )
+
+#define FATAL_IMPL_HAS_ARGS_1(...) \
+  FATAL_ARG_1(FATAL_IMPL_HAS_ARGS_2 __VA_ARGS__)()
+
+#define FATAL_IMPL_HAS_ARGS_2(...) \
+  FATAL_IMPL_HAS_ARGS_3 __VA_ARGS__
+
+#define FATAL_IMPL_HAS_ARGS_3(...) \
+  0
 
 ///////////////////////
 // FATAL_CONDITIONAL //
 ///////////////////////
 
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 #define FATAL_CONDITIONAL(condition) \
-  FATAL_CONDITIONAL_IMPL(FATAL_BOOL(condition))
+  FATAL_IMPL_CONDITIONAL(FATAL_BOOL(condition))
+
+#define FATAL_IMPL_CONDITIONAL(condition) \
+  FATAL_CAT(FATAL_CONDITIONAL_IMPL_, condition)
+
+#define FATAL_CONDITIONAL_IMPL_1(...) \
+  __VA_ARGS__ FATAL_IGNORE
+
+#define FATAL_CONDITIONAL_IMPL_0(...) \
+  FATAL_IDENTITY
 
 ///////////////////
 // FATAL_DEFER_n //
 ///////////////////
 
-#define FATAL_DEFER_1(x) x FATAL_EMPTY()
-#define FATAL_DEFER_2(x) x FATAL_EMPTY FATAL_EMPTY()()
-#define FATAL_DEFER_3(x) x FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY()()()
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_DEFER_1(x) \
+  x FATAL_EMPTY()
+
+#define FATAL_DEFER_2(x) \
+  x FATAL_EMPTY FATAL_EMPTY()()
+
+#define FATAL_DEFER_3(x) \
+  x FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY()()()
+
 #define FATAL_DEFER_4(x) \
   x FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY()()()()
+
 #define FATAL_DEFER_5(x) \
   x FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY()()()()()
+
 #define FATAL_DEFER_6(x) \
   x FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY \
-    FATAL_EMPTY()()()()()()
+  FATAL_EMPTY()()()()()()
+
 #define FATAL_DEFER_7(x) \
   x FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY \
-    FATAL_EMPTY FATAL_EMPTY()()()()()()()
+  FATAL_EMPTY FATAL_EMPTY()()()()()()()
+
 #define FATAL_DEFER_8(x) \
   x FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY \
-    FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY()()()()()()()()
+  FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY()()()()()()()()
+
 #define FATAL_DEFER_9(x) \
   x FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY \
-    FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY()()()()()()()()()
+  FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY FATAL_EMPTY()()()()()()()()()
 
 ////////////////
 // FATAL_EVAL //
 ////////////////
 
-#define FATAL_EVAL(...) FATAL_EVAL_IMPL_1024(__VA_ARGS__)
-
-//////////////////////
-// FATAL_SIMPLE_MAP //
-//////////////////////
-
-#define FATAL_SIMPLE_MAP(Fn, x, ...) \
-  Fn(x) \
-  FATAL_CONDITIONAL(FATAL_HAS_ARGS(__VA_ARGS__))( \
-    FATAL_DEFER_2(FATAL_SIMPLE_MAP_IMPL)()(Fn, __VA_ARGS__) \
-  )()
-
-///////////////
-// FATAL_MAP //
-///////////////
-
-#define FATAL_MAP(Fn, arg, ...) \
-  FATAL_MAP_IMPL(Fn, arg, 1, 0, __VA_ARGS__)
-
-//////////////////
-// FATAL_TO_STR //
-//////////////////
-
-#define FATAL_TO_STR(...) FATAL_TO_STR_IMPL(__VA_ARGS__)
-
-///////////////
-// FATAL_UID //
-///////////////
-
-#define FATAL_UID(Prefix) FATAL_CAT(Prefix, FATAL_CAT(_, __LINE__))
-
-////////////////////////////
-// IMPLEMENTATION DETAILS //
-////////////////////////////
-
-//////////////////
-// FATAL_TO_STR //
-//////////////////
-
-#define FATAL_TO_STR_IMPL(...) #__VA_ARGS__
-
-///////////////
-// FATAL_CAT //
-///////////////
-
-#define FATAL_CAT_IMPL(LHS, RHS) LHS ## RHS
-
-///////////////
-// FATAL_NOT //
-///////////////
-
-#define FATAL_NOT_IMPL(...) FATAL_ARG_2(__VA_ARGS__, 0)
-#define FATAL_NOT_IMPL_0 FATAL_NOT_WHEN_FALSE_IMPL()
-#define FATAL_NOT_WHEN_FALSE_IMPL() ~, 1
-
-////////////////////
-// FATAL_HAS_ARGS //
-////////////////////
-
-#define FATAL_HAS_ARGS_IMPL() 0
-
-///////////////////////
-// FATAL_CONDITIONAL //
-///////////////////////
-
-#define FATAL_CONDITIONAL_IMPL(condition) \
-  FATAL_CAT(FATAL_CONDITIONAL_IMPL_, condition)
-#define FATAL_CONDITIONAL_IMPL_1(...) __VA_ARGS__ FATAL_IGNORE
-#define FATAL_CONDITIONAL_IMPL_0(...) FATAL_IDENTITY
-
-////////////////
-// FATAL_EVAL //
-////////////////
-
-#define FATAL_EVAL_IMPL_1024(...) \
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_EVAL(...) \
   FATAL_EVAL_IMPL_512(FATAL_EVAL_IMPL_512(__VA_ARGS__))
+
 #define FATAL_EVAL_IMPL_512(...) \
   FATAL_EVAL_IMPL_256(FATAL_EVAL_IMPL_256(__VA_ARGS__))
+
 #define FATAL_EVAL_IMPL_256(...) \
   FATAL_EVAL_IMPL_128(FATAL_EVAL_IMPL_128(__VA_ARGS__))
+
 #define FATAL_EVAL_IMPL_128(...) \
   FATAL_EVAL_IMPL_64(FATAL_EVAL_IMPL_64(__VA_ARGS__))
+
 #define FATAL_EVAL_IMPL_64(...) \
   FATAL_EVAL_IMPL_32(FATAL_EVAL_IMPL_32(__VA_ARGS__))
+
 #define FATAL_EVAL_IMPL_32(...) \
   FATAL_EVAL_IMPL_16(FATAL_EVAL_IMPL_16(__VA_ARGS__))
+
 #define FATAL_EVAL_IMPL_16(...) \
   FATAL_EVAL_IMPL_8(FATAL_EVAL_IMPL_8(__VA_ARGS__))
+
 #define FATAL_EVAL_IMPL_8(...) \
   FATAL_EVAL_IMPL_4(FATAL_EVAL_IMPL_4(__VA_ARGS__))
+
 #define FATAL_EVAL_IMPL_4(...) \
   FATAL_EVAL_IMPL_2(FATAL_EVAL_IMPL_2(__VA_ARGS__))
+
 #define FATAL_EVAL_IMPL_2(...) \
   FATAL_EVAL_IMPL_1(FATAL_EVAL_IMPL_1(__VA_ARGS__))
+
 #define FATAL_EVAL_IMPL_1(...) \
   __VA_ARGS__
 
@@ -202,20 +371,66 @@ namespace fatal {
 // FATAL_SIMPLE_MAP //
 //////////////////////
 
-#define FATAL_SIMPLE_MAP_IMPL() FATAL_SIMPLE_MAP
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_SIMPLE_MAP(Fn, ...) \
+  FATAL_MAP(FATAL_IMPL_SIMPLE_MAP, Fn, __VA_ARGS__)
+
+#define FATAL_IMPL_SIMPLE_MAP(Fn, is_first, index, ...) \
+  Fn(__VA_ARGS__)
 
 ///////////////
 // FATAL_MAP //
 ///////////////
 
-#define FATAL_MAP_IMPL(Fn, arg, is_first, index, x, ...) \
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_MAP(Fn, arg, ...) \
+  FATAL_EVAL(FATAL_IMPL_MAP(Fn, arg, 1, 0, __VA_ARGS__))
+
+#define FATAL_IMPL_MAP(Fn, arg, is_first, index, x, ...) \
   Fn(arg, is_first, index, x) \
   FATAL_CONDITIONAL(FATAL_HAS_ARGS(__VA_ARGS__))( \
-    FATAL_DEFER_2(FATAL_MAP_RECURSE_IMPL)()\
+    FATAL_DEFER_2(FATAL_IMPL_MAP_RECURSE)()\
       (Fn, arg, 0, (index + 1), __VA_ARGS__) \
   )()
 
-#define FATAL_MAP_RECURSE_IMPL() FATAL_MAP_IMPL
+#define FATAL_IMPL_MAP_RECURSE() \
+  FATAL_IMPL_MAP
+
+
+//////////////////
+// FATAL_TO_STR //
+//////////////////
+
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_TO_STR(...) \
+  FATAL_IMPL_TO_STR(__VA_ARGS__)
+
+#define FATAL_IMPL_TO_STR(...) \
+  # __VA_ARGS__
+
+///////////////
+// FATAL_UID //
+///////////////
+
+/**
+ * TODO: DOCUMENT
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+#define FATAL_UID(Prefix) \
+  FATAL_CAT(Prefix, FATAL_CAT(_, __LINE__))
 
 } // namespace fatal {
 
--- fatal/test/preprocessor_test.cpp
+++ fatal/test/preprocessor_test.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2014, Facebook, Inc.
+ *  Copyright (c) 2015, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -11,7 +11,19 @@
 
 #include <fatal/test/driver.h>
 
+#include <array>
+#include <vector>
+
 namespace fatal {
+
+template <typename T, typename... Args>
+std::vector<typename std::decay<T>::type> to_vector(T &&value, Args &&...args) {
+  return { std::forward<T>(value), std::forward<Args>(args)... };
+}
+
+/////////////////
+// FATAL_EMPTY //
+/////////////////
 
 TEST(preprocessor, fatal_empty) {
   int FATAL_EMPTY() x = 1;
@@ -23,6 +35,10 @@ TEST(preprocessor, fatal_empty) {
   EXPECT_EQ(2, x FATAL_EMPTY());
   EXPECT_EQ(3, x + FATAL_EMPTY() 1);
 }
+
+//////////////////
+// FATAL_IGNORE //
+//////////////////
 
 TEST(preprocessor, fatal_ignore) {
   int FATAL_IGNORE(~) x = 1;
@@ -37,6 +53,10 @@ TEST(preprocessor, fatal_ignore) {
   EXPECT_EQ(3, x + 1);
 }
 
+////////////////////
+// FATAL_IDENTITY //
+////////////////////
+
 TEST(preprocessor, fatal_identity) {
   int x = FATAL_IDENTITY(1);
   EXPECT_EQ(1, x);
@@ -46,6 +66,10 @@ TEST(preprocessor, fatal_identity) {
 
   EXPECT_EQ(3, x FATAL_IDENTITY(+ 1));
 }
+
+///////////////
+// FATAL_CAT //
+///////////////
 
 TEST(preprocessor, fatal_cat) {
   int a = 1;
@@ -74,12 +98,21 @@ TEST(preprocessor, fatal_cat) {
   EXPECT_EQ(abc, FATAL_CAT(FATAL_CAT(a, b), c));
 }
 
+///////////////
+// FATAL_NOT //
+///////////////
+
 TEST(preprocessor, fatal_not) {
+  EXPECT_EQ(1, FATAL_NOT());
   EXPECT_EQ(1, FATAL_NOT(0));
   EXPECT_EQ(0, FATAL_NOT(1));
   EXPECT_EQ(0, FATAL_NOT(56));
   EXPECT_EQ(0, FATAL_NOT(arg));
 }
+
+////////////////
+// FATAL_BOOL //
+////////////////
 
 TEST(preprocessor, fatal_bool) {
   EXPECT_EQ(0, FATAL_BOOL(0));
@@ -88,23 +121,183 @@ TEST(preprocessor, fatal_bool) {
   EXPECT_EQ(1, FATAL_BOOL(arg));
 }
 
-TEST(preprocessor, fatal_arg_n) {
-  EXPECT_EQ(1, FATAL_ARG_1(1, 2, 3, 4, 5));
-  EXPECT_EQ(2, FATAL_ARG_2(1, 2, 3, 4, 5));
-  EXPECT_EQ(3, FATAL_ARG_3(1, 2, 3, 4, 5));
-  EXPECT_EQ(4, FATAL_ARG_4(1, 2, 3, 4, 5));
-  EXPECT_EQ(5, FATAL_ARG_5(1, 2, 3, 4, 5));
+////////////////////////////
+// FATAL_IS_PARENTHESIZED //
+////////////////////////////
+
+TEST(preprocessor, fatal_is_parenthesized) {
+# define TEST_IMPL(...) \
+    FATAL_IS_PARENTHESIZED(__VA_ARGS__)
+
+  EXPECT_EQ(0, TEST_IMPL(0));
+  EXPECT_EQ(1, TEST_IMPL((0)));
+  EXPECT_EQ(0, TEST_IMPL(0, 2));
+  EXPECT_EQ(1, TEST_IMPL((0, 2)));
+  EXPECT_EQ(1, TEST_IMPL((0), (2)));
+  EXPECT_EQ(1, TEST_IMPL(((0), (2))));
+
+  EXPECT_EQ(0, TEST_IMPL(1));
+  EXPECT_EQ(1, TEST_IMPL((1)));
+  EXPECT_EQ(0, TEST_IMPL(1, 2));
+  EXPECT_EQ(1, TEST_IMPL((1, 2)));
+  EXPECT_EQ(1, TEST_IMPL((1), (2)));
+  EXPECT_EQ(1, TEST_IMPL(((1), (2))));
+
+# undef TEST_IMPL
 }
+
+//////////////////////////
+// FATAL_UNPARENTHESIZE //
+//////////////////////////
+
+TEST(preprocessor, fatal_unparenthesize) {
+# define TEST_IMPL(...) \
+    FATAL_TO_STR(FATAL_UNPARENTHESIZE(__VA_ARGS__))
+
+  EXPECT_EQ("0", TEST_IMPL(0));
+  EXPECT_EQ("0", TEST_IMPL((0)));
+  EXPECT_EQ("0, 2", TEST_IMPL(0, 2));
+  EXPECT_EQ("0, 2", TEST_IMPL((0, 2)));
+  EXPECT_EQ("0, (2)", TEST_IMPL((0), (2)));
+  EXPECT_EQ("(0), (2)", TEST_IMPL(((0), (2))));
+
+  EXPECT_EQ("1", TEST_IMPL(1));
+  EXPECT_EQ("1", TEST_IMPL((1)));
+  EXPECT_EQ("1, 2", TEST_IMPL(1, 2));
+  EXPECT_EQ("1, 2", TEST_IMPL((1, 2)));
+  EXPECT_EQ("1, (2)", TEST_IMPL((1), (2)));
+  EXPECT_EQ("(1), (2)", TEST_IMPL(((1), (2))));
+
+# undef TEST_IMPL
+}
+
+/////////////////
+// FATAL_ARG_n //
+/////////////////
+
+TEST(preprocessor, fatal_arg_n) {
+# define TEST_IMPL(Fn) \
+  Fn(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+
+  EXPECT_EQ(1, TEST_IMPL(FATAL_ARG_1));
+  EXPECT_EQ(2, TEST_IMPL(FATAL_ARG_2));
+  EXPECT_EQ(3, TEST_IMPL(FATAL_ARG_3));
+  EXPECT_EQ(4, TEST_IMPL(FATAL_ARG_4));
+  EXPECT_EQ(5, TEST_IMPL(FATAL_ARG_5));
+  EXPECT_EQ(6, TEST_IMPL(FATAL_ARG_6));
+  EXPECT_EQ(7, TEST_IMPL(FATAL_ARG_7));
+  EXPECT_EQ(8, TEST_IMPL(FATAL_ARG_8));
+  EXPECT_EQ(9, TEST_IMPL(FATAL_ARG_9));
+  EXPECT_EQ(10, TEST_IMPL(FATAL_ARG_10));
+  EXPECT_EQ(11, TEST_IMPL(FATAL_ARG_11));
+  EXPECT_EQ(12, TEST_IMPL(FATAL_ARG_12));
+  EXPECT_EQ(13, TEST_IMPL(FATAL_ARG_13));
+  EXPECT_EQ(14, TEST_IMPL(FATAL_ARG_14));
+  EXPECT_EQ(15, TEST_IMPL(FATAL_ARG_15));
+  EXPECT_EQ(16, TEST_IMPL(FATAL_ARG_16));
+
+# undef TEST_IMPL
+}
+
+///////////////////////
+// FATAL_SKIP_n_ARGS //
+///////////////////////
+
+TEST(preprocessor, fatal_skip_n_args) {
+# define TEST_IMPL(Fn) \
+  Fn(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+
+  EXPECT_EQ(
+    "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_1_ARG))
+  );
+  EXPECT_EQ(
+    "2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_2_ARGS))
+  );
+  EXPECT_EQ(
+    "3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_3_ARGS))
+  );
+  EXPECT_EQ(
+    "4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_4_ARGS))
+  );
+  EXPECT_EQ(
+    "5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_5_ARGS))
+  );
+  EXPECT_EQ(
+    "6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_6_ARGS))
+  );
+  EXPECT_EQ(
+    "7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_7_ARGS))
+  );
+  EXPECT_EQ(
+    "8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_8_ARGS))
+  );
+  EXPECT_EQ(
+    "9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_9_ARGS))
+  );
+  EXPECT_EQ(
+    "10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_10_ARGS))
+  );
+  EXPECT_EQ(
+    "11, 12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_11_ARGS))
+  );
+  EXPECT_EQ(
+    "12, 13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_12_ARGS))
+  );
+  EXPECT_EQ(
+    "13, 14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_13_ARGS))
+  );
+  EXPECT_EQ(
+    "14, 15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_14_ARGS))
+  );
+  EXPECT_EQ(
+    "15, 16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_15_ARGS))
+  );
+  EXPECT_EQ(
+    "16, 17, 18, 19, 20",
+    FATAL_TO_STR(TEST_IMPL(FATAL_SKIP_16_ARGS))
+  );
+
+# undef TEST_IMPL
+}
+
+////////////////////
+// FATAL_HAS_ARGS //
+////////////////////
 
 TEST(preprocessor, fatal_has_args) {
   EXPECT_EQ(0, FATAL_HAS_ARGS());
   EXPECT_EQ(0, FATAL_HAS_ARGS( ));
+  EXPECT_EQ(0, FATAL_HAS_ARGS(()));
   EXPECT_EQ(0, FATAL_HAS_ARGS(,));
   EXPECT_EQ(1, FATAL_HAS_ARGS(0));
   EXPECT_EQ(1, FATAL_HAS_ARGS(1));
   EXPECT_EQ(1, FATAL_HAS_ARGS(56));
   EXPECT_EQ(1, FATAL_HAS_ARGS(arg));
+  EXPECT_EQ(1, FATAL_HAS_ARGS((x)));
+  EXPECT_EQ(1, FATAL_HAS_ARGS((x), (y)));
+  EXPECT_EQ(1, FATAL_HAS_ARGS((x, y)));
+  EXPECT_EQ(1, FATAL_HAS_ARGS((x, y), (z)));
+  EXPECT_EQ(1, FATAL_HAS_ARGS((x, y), (z, ~)));
 }
+
+///////////////////////
+// FATAL_CONDITIONAL //
+///////////////////////
 
 TEST(preprocessor, fatal_conditional) {
   EXPECT_EQ(20, FATAL_CONDITIONAL(0)(10)(20));
@@ -112,6 +305,10 @@ TEST(preprocessor, fatal_conditional) {
   EXPECT_EQ(10, FATAL_CONDITIONAL(56)(10)(20));
   EXPECT_EQ(10, FATAL_CONDITIONAL(arg)(10)(20));
 }
+
+///////////////////
+// FATAL_DEFER_n //
+///////////////////
 
 #define TEST_IMPL(...) |__VA_ARGS__|
 
@@ -304,7 +501,11 @@ TEST(preprocessor, fatal_defer_9) {
   );
 }
 
-# undef TEST_IMPL
+#undef TEST_IMPL
+
+////////////////
+// FATAL_EVAL //
+////////////////
 
 TEST(preprocessor, fatal_eval) {
 # define TEST_IMPL(...) TEST_IMPL_1 FATAL_EMPTY() (__VA_ARGS__)
@@ -341,50 +542,73 @@ TEST(preprocessor, fatal_eval) {
 # undef TEST_IMPL
 }
 
+//////////////////////
+// FATAL_SIMPLE_MAP //
+//////////////////////
+
 TEST(preprocessor, fatal_simple_map) {
 # define TEST_IMPL(x) + x
-  EXPECT_EQ(15, 0 FATAL_EVAL(FATAL_SIMPLE_MAP(TEST_IMPL, 1, 2, 3, 4, 5)));
+  EXPECT_EQ(15, 0 FATAL_SIMPLE_MAP(TEST_IMPL, 1, 2, 3, 4, 5));
 # undef TEST_IMPL
 
-  EXPECT_EQ(
-    15,
-    FATAL_EVAL(FATAL_SIMPLE_MAP(FATAL_IDENTITY, 1, +, 2, +, 3, +, 4, +, 5))
-  );
+  EXPECT_EQ(15, FATAL_SIMPLE_MAP(FATAL_IDENTITY, 1, +, 2, +, 3, +, 4, +, 5));
 
   EXPECT_EQ(
     "1 2 3 4 5",
-    FATAL_TO_STR(FATAL_EVAL(FATAL_SIMPLE_MAP(FATAL_IDENTITY, 1, 2, 3, 4, 5)))
+    FATAL_TO_STR(FATAL_SIMPLE_MAP(FATAL_IDENTITY, 1, 2, 3, 4, 5))
   );
+
+# define TEST_IMPL(...) / __VA_ARGS__ |
+  EXPECT_EQ(
+    "/ (1, 2, 3) | / (4, 5, 6) | / (7, 8, 9) |",
+    FATAL_TO_STR(
+      FATAL_SIMPLE_MAP(TEST_IMPL, (1, 2, 3), (4, 5, 6), (7, 8, 9))
+    )
+  );
+# undef TEST_IMPL
+
+# define TEST_IMPL(...)  to_vector __VA_ARGS__,
+
+  EXPECT_EQ(
+    to_vector(
+      to_vector(1, 2, 3), to_vector(4, 5, 6), to_vector(7, 8, 9),
+      std::vector<int>{}
+    ),
+    to_vector(
+      FATAL_SIMPLE_MAP(TEST_IMPL, (1, 2, 3), (4, 5, 6), (7, 8, 9))
+      std::vector<int>{}
+    )
+  );
+
+# undef TEST_IMPL
 }
+
+///////////////
+// FATAL_MAP //
+///////////////
 
 TEST(preprocessor, fatal_map) {
 # define TEST_IMPL(arg, is_first, index, x) + x
-  EXPECT_EQ(15, 0 FATAL_EVAL(FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5)));
+  EXPECT_EQ(15, 0 FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5));
 # undef TEST_IMPL
 
 # define TEST_IMPL(arg, is_first, index, x) x
-  EXPECT_EQ(
-    15,
-    FATAL_EVAL(FATAL_MAP(TEST_IMPL, ~, 1, +, 2, +, 3, +, 4, +, 5))
-  );
+  EXPECT_EQ(15, FATAL_MAP(TEST_IMPL, ~, 1, +, 2, +, 3, +, 4, +, 5));
 # undef TEST_IMPL
 
 # define TEST_IMPL(arg, is_first, index, x) x
-  EXPECT_EQ(
-    "1 2 3 4 5",
-    FATAL_TO_STR(FATAL_EVAL(FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5)))
-  );
+  EXPECT_EQ("1 2 3 4 5", FATAL_TO_STR(FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5)));
 # undef TEST_IMPL
 
 # define TEST_IMPL(arg, is_first, index, x) \
   FATAL_CONDITIONAL(is_first)(x)(+ x)
-  EXPECT_EQ(15, FATAL_EVAL(FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5)));
+  EXPECT_EQ(15, FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5));
 # undef TEST_IMPL
 
 # define TEST_IMPL(arg, is_first, index, x) / arg is_first x |
   EXPECT_EQ(
     "/ ~ 1 1 | / ~ 0 2 | / ~ 0 3 | / ~ 0 4 | / ~ 0 5 |",
-    FATAL_TO_STR(FATAL_EVAL(FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5)))
+    FATAL_TO_STR(FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5))
   );
 # undef TEST_IMPL
 
@@ -395,7 +619,7 @@ TEST(preprocessor, fatal_map) {
       a[index] = x; \
     } while (false);
 
-  FATAL_EVAL(FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5));
+  FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5);
 # undef TEST_IMPL
 
   std::vector<int> v;
@@ -404,7 +628,7 @@ TEST(preprocessor, fatal_map) {
       v.push_back(x); \
     } while (false);
 
-  FATAL_EVAL(FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5));
+  FATAL_MAP(TEST_IMPL, ~, 1, 2, 3, 4, 5);
 # undef TEST_IMPL
 
   ASSERT_EQ(a.size(), v.size());
@@ -413,13 +637,39 @@ TEST(preprocessor, fatal_map) {
     EXPECT_EQ(i + 1, a[i]);
     EXPECT_EQ(i + 1, v[i]);
   }
+
+# define TEST_IMPL(arg, is_first, index, ...) / __VA_ARGS__ |
+  EXPECT_EQ(
+    "/ (1, 2, 3) | / (4, 5, 6) | / (7, 8, 9) |",
+    FATAL_TO_STR(FATAL_MAP(TEST_IMPL, ~, (1, 2, 3), (4, 5, 6), (7, 8, 9)))
+  );
+# undef TEST_IMPL
+
+# define TEST_IMPL(arg, is_first, index, ...) \
+  FATAL_CONDITIONAL(is_first)()(,) \
+  to_vector __VA_ARGS__
+
+  EXPECT_EQ(
+    to_vector(to_vector(1, 2, 3), to_vector(4, 5, 6), to_vector(7, 8, 9)),
+    to_vector(FATAL_MAP(TEST_IMPL, ~, (1, 2, 3), (4, 5, 6), (7, 8, 9)))
+  );
+
+# undef TEST_IMPL
 }
+
+//////////////////
+// FATAL_TO_STR //
+//////////////////
 
 TEST(preprocessor, fatal_to_str) {
   EXPECT_EQ(FATAL_TO_STR(x), "x");
   EXPECT_EQ(FATAL_TO_STR(xyz), "xyz");
   EXPECT_EQ(FATAL_TO_STR(x y z), "x y z");
 }
+
+///////////////
+// FATAL_UID //
+///////////////
 
 struct uid_tester {
   uid_tester(int &x): x_(x) { ++x_; }
