484cbea134dade0579bc082f0c022c90b460e84f
Marcelo Juchem <marcelo@fb.com>
2015-02-26 18:04:20 -0800

    - fixing redundant passes in test.sh

--- test.sh
+++ test.sh
@@ -8,11 +8,17 @@ fi
 
 if [ -z "$1" ]; then
   set -x
-  for cc in clang++-3.5 g++-4.8 g++-4.9 clang++-3.4; do
+  if [ -z "$USE_CC" ]; then
+    for cc in clang++-3.5 g++-4.8 g++-4.9 clang++-3.4; do
+      for f in `find . -name \*_test.cpp`; do
+        USE_CC="$cc" NO_CLEAR=true "$0" "$f" "$@"
+      done
+    done
+  else
     for f in `find . -name \*_test.cpp`; do
       USE_CC="$cc" NO_CLEAR=true "$0" "$f" "$@"
     done
-  done
+  fi
 elif [ -z "$2" ] && [ -e "$1" ]; then
   test_path="`basename "$1" .cpp`"
   if ! [ -e "$1" ]; then
