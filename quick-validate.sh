7ced498057c1a26405aa235af28346def5642d21
Marcelo Juchem <marcelo@fb.com>
2015-03-26 14:55:34 -0700

    - adding more compilers to quick-validate

--- quick-validate.sh
+++ quick-validate.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-for cc in clang++-3.4 g++-4.8; do
-  if [ "$NO_CLEAR" != "true" ]; then
-    ./lclear.sh >&2
-  fi
+if [ "$NO_CLEAR" != "true" ]; then
+  ./lclear.sh >&2
+fi
 
+for cc in clang++-3.5; do
+  NO_CLEAR=true USE_CC="$cc" ./validate.sh "$@"
+done
+
+for cc in g++-4.8 clang++-3.4 g++-4.9; do
   NO_CLEAR=true USE_CC="$cc" ./validate.sh nobuild "$@"
 done
