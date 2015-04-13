0bc28809c873e858563fa34474039713e2fe0f31
Marcelo Juchem <marcelo@fb.com>
2015-04-13 13:52:22 -0700

    - adding an internal testing utility for generating random data

---
+++ fatal/test/random_data.h
@@ -0,0 +1,134 @@
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_test_random_data_h
+#define FATAL_INCLUDE_fatal_test_random_data_h
+
+#include <iterator>
+#include <random>
+#include <string>
+#include <vector>
+
+#include <cassert>
+
+namespace fatal {
+namespace detail {
+namespace random_data_impl {
+
+// TODO: REMOVE DEFAULT ALPHABET
+static auto const alphabet = "0123456789"
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  "abcdefghijklmnopqrstuvwxyz";
+
+static auto const alphabet_size = sizeof(alphabet) / sizeof(*alphabet);
+
+} // namespace random_data_impl {
+} // namespace detail {
+
+struct random_data {
+  random_data(): rng_([] { std::random_device r; return r(); }()) {}
+
+  template <typename Iterator>
+  void string(
+    Iterator begin,
+    Iterator end,
+    char const *alphabet,
+    std::size_t alphabet_size
+  ) {
+    assert(alphabet);
+    assert(alphabet_size > 0);
+
+    for (; begin != end; ++begin) {
+      auto const index = rng_() % alphabet_size;
+      assert(index >= 0);
+      *begin = alphabet[index];
+    }
+  }
+
+  template <typename Iterator>
+  void string(Iterator begin, Iterator end) {
+    string(
+      begin,
+      end,
+      detail::random_data_impl::alphabet,
+      detail::random_data_impl::alphabet_size
+    );
+  }
+
+  std::string string(
+    std::size_t size,
+    char const *alphabet,
+    std::size_t alphabet_size
+  ) {
+    std::string result;
+    result.reserve(size);
+    string(result.begin(), result.end(), alphabet, alphabet_size);
+    return result;
+  }
+
+  std::string string(std::size_t size) {
+    std::string result(size, '\0');
+    string(result.begin(), result.end());
+    return result;
+  }
+  
+  template <typename T>
+  std::size_t chop(std::size_t size, T &&fn) {
+    std::size_t chunks = 0;
+
+    for (std::size_t offset = 0; offset < size; ++chunks) {
+      std::size_t const chunk = rng_() % (size - offset) + 1;
+      assert(chunk <= size - offset);
+      fn(offset, chunk);
+      offset += chunk;
+      assert(offset <= size);
+    }
+
+    return chunks;
+  }
+
+  template <typename T, typename Iterator>
+  std::size_t chop(Iterator begin, Iterator const end, T &&fn) {
+    assert(begin <= end);
+
+    return chop(
+      std::distance(begin, end),
+      [&](std::size_t offset, std::size_t size) {
+        auto const b = std::next(begin, offset);
+        assert(begin < end);
+        auto const e = std::next(b, size);
+        assert(e <= end);
+
+        fn(b, e);
+      }
+    );
+  }
+  
+  std::vector<std::string> chop(std::string const &s) {
+    std::vector<std::string> result;
+
+    auto const chunks = chop(
+      s.begin(), s.end(),
+      [&](std::string::const_iterator begin, std::string::const_iterator end) {
+        assert(begin < end);
+        result.emplace_back(begin, end);
+      }
+    );
+
+    assert(result.size() == chunks);
+    return result;
+  }
+
+private:
+  std::mt19937 rng_;
+};
+
+} // namespace fatal {
+
+#endif // FATAL_INCLUDE_fatal_test_random_data_h
