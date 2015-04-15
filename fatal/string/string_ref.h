f48fc85d0ea485e750e9168a716f6dd2873bf960
Marcelo Juchem <marcelo@fb.com>
2015-04-15 02:15:39 -0700

    - adding string_ref::{default-ctor|value_type|find|split_step|operator ==|operator !=|operator <|operator >|operator <=|operator >=|operator <<(ostream)}

--- fatal/string/string_ref.h
+++ fatal/string/string_ref.h
@@ -12,38 +12,43 @@
 
 #include <fatal/math/hash.h>
 
+#include <algorithm>
 #include <iterator>
 
+#include <cassert>
 #include <cstring>
 
 namespace fatal {
 
 struct string_ref {
+  using value_type = char;
   using size_type = std::size_t;
   using const_iterator = char const *;
 
-  string_ref(char const *begin, char const *end):
+  string_ref(): begin_(nullptr), end_(nullptr) {}
+
+  string_ref(const_iterator begin, const_iterator end):
     begin_(begin),
     end_(end)
   {
     assert(begin_ <= end_);
   }
 
-  string_ref(char const *s, std::size_t size):
+  string_ref(const_iterator s, std::size_t size):
     begin_(s),
     end_(std::next(s, size))
   {
     assert(begin_ <= end_);
   }
 
-  explicit string_ref(char *s):
+  explicit string_ref(value_type *s):
     begin_(s),
     end_(std::next(s, std::strlen(s)))
   {
     assert(begin_ <= end_);
   }
 
-  explicit string_ref(char const *s):
+  explicit string_ref(value_type const *s):
     begin_(s),
     end_(std::next(s, std::strlen(s)))
   {
@@ -51,22 +56,22 @@ struct string_ref {
   }
 
   template <std::size_t N>
-  explicit string_ref(char const (&s)[N]):
+  explicit string_ref(value_type const (&s)[N]):
     begin_(s),
     end_(std::strlen(s))
   {
     assert(begin_ <= end_);
   }
 
-  explicit string_ref(char const &c):
+  explicit string_ref(value_type const &c):
     begin_(std::addressof(c)),
     end_(std::next(std::addressof(c)))
   {
     assert(begin_ <= end_);
   }
 
-  template <typename T, typename = safe_overload_t<string_ref, T>>
-  explicit string_ref(T &&s):
+  template <typename U, typename = safe_overload_t<string_ref, U>>
+  explicit string_ref(U &&s):
     begin_(s.data()),
     end_(std::next(s.data(), s.size()))
   {
@@ -77,6 +82,24 @@ struct string_ref {
     assert(offset <= size());
     assert(end <= size());
     return string_ref(std::next(begin_, offset), std::next(begin_, end));
+  }
+
+  const_iterator find(value_type needle) const {
+    return find(needle, begin_);
+  }
+
+  const_iterator find(value_type needle, const_iterator offset) const {
+    assert(begin_ <= offset);
+    assert(offset <= end_);
+    return std::find(offset, end_, needle);
+  }
+
+  template <typename U>
+  string_ref split_step(U &&delimiter) {
+    string_ref result(begin_, find(std::forward<U>(delimiter)));
+    begin_ = result.end() == end_ ? end_ : std::next(result.end());
+    assert(begin_ <= end_);
+    return result;
   }
 
   void advance_to(const_iterator i) {
@@ -95,7 +118,7 @@ struct string_ref {
     assert(begin_ <= end_);
   }
 
-  char const *data() const { return begin_; }
+  const_iterator data() const { return begin_; }
 
   void clear() { begin_ = end_; }
 
@@ -164,9 +187,41 @@ struct string_ref {
     return copy;
   }
 
-  char operator *() const {
+  value_type operator *() const {
     assert(begin_ <= end_);
     return *begin_;
+  }
+
+  bool operator ==(string_ref rhs) const {
+    return size() == rhs.size() && !std::strncmp(begin_, rhs.begin_, size());
+  }
+
+  template <typename U, typename = safe_overload_t<string_ref, U>>
+  bool operator ==(U &&rhs) const {
+    return *this == string_ref(std::forward<U>(rhs));
+  }
+
+  template <typename U>
+  bool operator !=(U &&rhs) const { return !(*this == std::forward<U>(rhs)); }
+
+  bool operator <(string_ref rhs) const {
+    auto const result = std::strncmp(
+      begin_, rhs.begin_, std::min(size(), rhs.size())
+    );
+
+    return result < 0 || (!result && size() < rhs.size());
+  }
+
+  template <typename U, typename = safe_overload_t<string_ref, U>>
+  bool operator <(U &&rhs) const {
+    return *this < string_ref(std::forward<U>(rhs));
+  }
+
+  bool operator >(string_ref rhs) const { return rhs < *this; }
+
+  template <typename U, typename = safe_overload_t<string_ref, U>>
+  bool operator >(U &&rhs) const {
+    return *this > string_ref(std::forward<U>(rhs));
   }
 
   explicit operator bool() const { return begin_ < end_; }
@@ -177,6 +232,65 @@ private:
   const_iterator begin_;
   const_iterator end_;
 };
+
+/////////////////
+// operator == //
+/////////////////
+
+template <typename T, typename = safe_overload_t<string_ref, T>>
+bool operator ==(T const &lhs, string_ref rhs) { return rhs == lhs; }
+
+////////////////
+// operator < //
+////////////////
+
+template <typename T, typename = safe_overload_t<string_ref, T>>
+bool operator <(T const &lhs, string_ref rhs) { return rhs > lhs; }
+
+////////////////
+// operator > //
+////////////////
+
+template <typename T, typename = safe_overload_t<string_ref, T>>
+bool operator >(T const &lhs, string_ref rhs) { return rhs < lhs; }
+
+/////////////////
+// operator != //
+/////////////////
+
+template <typename T>
+bool operator !=(string_ref lhs, T const &rhs) { return !(lhs == rhs); }
+
+template <typename T, typename = safe_overload_t<string_ref, T>>
+bool operator !=(T const &lhs, string_ref rhs) { return !(rhs == lhs); }
+
+/////////////////
+// operator <= //
+/////////////////
+
+template <typename T>
+bool operator <=(string_ref lhs, T const &rhs) { return !(lhs > rhs); }
+
+template <typename T, typename = safe_overload_t<string_ref, T>>
+bool operator <=(T const &lhs, string_ref rhs) { return !(rhs < lhs); }
+
+/////////////////
+// operator >= //
+/////////////////
+
+template <typename T>
+bool operator >=(string_ref lhs, T const &rhs) { return !(lhs < rhs); }
+
+template <typename T, typename = safe_overload_t<string_ref, T>>
+bool operator >=(T const &lhs, string_ref rhs) { return !(rhs > lhs); }
+
+///////////////////////////////
+// operator <<(std::ostream) //
+///////////////////////////////
+
+std::ostream &operator <<(std::ostream &out, string_ref rhs) {
+  return out.write(rhs.data(), rhs.size());
+}
 
 } // namespace fatal {
 
