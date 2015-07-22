9c54b2b528711eabc0519ae503a802c8c2899c06
Marcelo Juchem <marcelo@fb.com>
2015-07-21 23:20:53 -0700

    - disabling building of benchmarks on contbuild to avoid breaking due to prefix_tree_benchmark's requirement on machine resources

--- contbuild.sh
+++ contbuild.sh
@@ -6,13 +6,15 @@ for cc in g++-4.8 g++-4.9 g++-5 clang-3.4 clang-3.5 clang-3.6 clang-3.7; do
   if which $cc; then
     echo "Package: $cc"
     dpkg -s $cc | grep '^Status: '
-    dpkg -L $cc | grep '/bin/'
   fi
 done
 
 for cc in g++-4.8 g++-4.9 g++-5; do
   if which $cc; then
     $cc --version
+    # TODO: split prefix_tree_benchmark into several benchmarks to avoid
+    #       breaking contbuild and remove the `nobuild` flag, then remove
+    #       this for loop and let `validate.sh` figure the compilers out
     USE_CC=$cc NO_CLEAR=true ./validate.sh nobuild
   else
     echo "no $cc installed, skipping validation"
@@ -22,7 +24,9 @@ done
 for cc in clang++-3.7 clang++-3.4 clang++ clang++-3.5 clang++-3.6; do
   if which $cc; then
     $cc --version
-    USE_CC=$cc NO_CLEAR=true ./validate.sh
+    # TODO: split prefix_tree_benchmark into several benchmarks to avoid
+    #       breaking contbuild and remove the `nobuild` flag
+    USE_CC=$cc NO_CLEAR=true ./validate.sh nobuild
   else
     echo "no $cc installed, skipping validation"
   fi
