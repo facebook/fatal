88451e65d5fbebdb8e4731a8f73422a63f0eba09
Marcelo Juchem <marcelo@fb.com>
2015-07-21 22:44:20 -0700

    - using debian-sid as the source for contbuild toolchains - adding clang 3.5, 3.6 and 3.7 to contbuild

--- contbuild.sh
+++ contbuild.sh
@@ -5,17 +5,17 @@ set -xe
 for cc in g++-4.8 g++-4.9 g++-5; do
   if which $cc; then
     $cc --version
+    USE_CC=$cc NO_CLEAR=true ./validate.sh nobuild
   else
     echo "no $cc installed, skipping validation"
   fi
-  USE_CC=$cc NO_CLEAR=true ./validate.sh nobuild
 done
 
 for cc in clang++-3.7 clang++-3.4 clang++ clang++-3.5 clang++-3.6; do
   if which $cc; then
     $cc --version
+    USE_CC=$cc NO_CLEAR=true ./validate.sh
   else
     echo "no $cc installed, skipping validation"
   fi
-  USE_CC=$cc NO_CLEAR=true ./validate.sh
 done
