e9352427ce3b285ca75ac536a699d450ed2f8b94
Marcelo Juchem <marcelo@fb.com>
2015-08-05 16:50:54 -0700

    - migrating lessons to new format

--- demo/tutorial/1_0-basics.cpp
+++ demo/tutorial/1_0-basics.cpp
@@ -7,178 +7,184 @@
  *  of patent rights can be found in the PATENTS file in the same directory.
  */
 
-#include <fatal/type/list.h>
+#include <fatal/lesson/driver.h>
 
-#include "./driver.h"
-
-namespace tutorial {
+namespace lesson {
 
 /**
- * This tutorial demonstrates how to achieve, in metaprogramming, something
- * similar to the notion of variables in procedural programming.
- *
  * @author: Marcelo Juchem <marcelo@fb.com>
  */
-FATAL_TUTORIAL(variables, part 1/2) {
-  /*
-    If you think about a duck typed language, we're used to declaring variables
-    by assigning a value to a name.
-  */
-  auto x = 10;
+LESSON(
+  "variables, part 1/2",
+  "This lesson demonstrates how to achieve, in metaprogramming, something "
+  "similar to the notion of variables in procedural programming."
+) {
+  COMMENT(
+    "If you think about a duck typed language, we're used to declaring "
+    "variables by assigning a value to a name."
+  );
+  CODE(
+    auto x = 10;
+  );
 
-  /*
-    Whenever we want to retrieve the value stored in that variable, we
-    reference it through its name:
-  */
-  PRINT << "x = " << x;
-  NEW_LINE;
+  COMMENT(
+    "Whenever we want to retrieve the value stored in that variable, we "
+    "reference it through its name:"
+  )
+  PRINT("x = ", x);
 
-  /*
-    A similar idiom can be achieved for metaprogramming. Take the following
-    line, for example:
-  */
-  using y = std::integral_constant<int, 10>;
+  COMMENT(
+    "A similar idiom can be achieved for metaprogramming. Take the following "
+    "line, for example:"
+  );
+  CODE(
+    using y = std::integral_constant<int, 10>;
+  );
 
-  /*
-    We're also assigning a value `10`, of type `int`, to the name `y`.
+  COMMENT(
+    "We're also assigning a value `10`, of type `int`, to the name `y`."
+    "\n\n"
+    "The difference is that instead of variables, we're using an alias to a "
+    "type."
+    "\n\n"
+    "Just like with variables, we can also retrieve what's stored in that "
+    "type alias:"
+  );
 
-    The difference is that instead of variables, we're using an alias to a
-    type.
+  PRINT("y = ", type_str<y>());
 
-    Just like with variables, we can also retrieve what's stored in that
-    type alias:
-  */
+  COMMENT(
+    "Note that we must use a helper function called `type_str()` to properly "
+    "convert types into their actual string representation."
+    "\n\n"
+    "The type `std::integral_constant`, in particular, can be used to represent "
+    "a constant value. Keep in mind, though, that types are immutable, therefore "
+    "we cannot change the value represented by the integral constant `y`."
+    "\n\n"
+    "We don't need to use `type_str()` here because the value itself is a "
+    "constant, not a type:"
+  );
 
-  PRINT << "y = " << type_str<y>();
-  NEW_LINE;
+  PRINT("y::value = ", y::value);
 
-  /*
-    Note that we must use a helper function called `type_str()` to properly
-    convert types into their actual string representation.
+  ILLEGAL(
+    "it's illegal to re-assign a value to a constant.",
+    y::value = 99;
+  );
 
-    The type `std::integral_constant`, in particular, can be used to represent
-    a constant value. Keep in mind, though, that types are immutable, therefore
-    we cannot change the value represented by the integral constant `y`.
+  COMMENT(
+    "That's the first thing you should notice about metaprogramming: we don't "
+    "manipulate values. We manipulate types."
+    "\n\n"
+    "Types only exist during compilation, so one could say metaprograms run "
+    "inside the compiler, at  compile time, as opposed to regular programs which "
+    "run at runtime, after compilation is done."
+    "\n\n"
+    "There are some types that can represent values, like "
+    "`std::integral_constant`, but that's not necessarily true for every type."
+    "\n\n"
+    "For instance, we could create an alias for the type `void`, or `int`, or "
+    "even `std::string`:"
+  );
 
-    We don't need to use `type_str()` here because the value itself is a
-    constant, not a type:
-  */
+  CODE(
+    using z = void;
+    using w = int;
+    using k = std::string;
+  );
 
-  PRINT << "y::value = " << y::value;
-  NEW_LINE;
+  COMMENT(
+    "A `int` or `std::string` runtime variable is able to store values, yes, but "
+    "as for the types themselves, `int` and `std::string`, they don't really "
+    "represent any values at compile time. Assigning values to them only make "
+    "sense at runtime."
+  );
 
-  // ERROR: it's illegal to re-assign a value to a constant. Uncomment the
-  // following line for a compilation error.
-  //y::value = 99;
+  PRINT("z = ", type_str<z>());
+  PRINT("w = ", type_str<w>());
+  PRINT("k = ", type_str<k>());
 
-  /*
-    That's the first thing you should notice about metaprogramming: we don't
-    manipulate values. We manipulate types.
+  COMMENT(
+    "The last thing to notice is that types are immutable. Once we assign a type "
+    "to an alias, that alias will always represent the same type."
+    "\n\n"
+    "That's a hint that metaprogramming in C++ works similarly to pure "
+    "functional programming."
+  );
 
-    Types only exist during compilation, so one could say metaprograms run
-    inside the compiler, at  compile time, as opposed to regular programs which
-    run at runtime, after compilation is done.
+  ILLEGAL(
+    "it's illegal to re-assign a type to an alias.",
+    w = long;
+  );
 
-    There are some types that can represent values, like
-    `std::integral_constant`, but that's not necessarily true for every type.
-
-    For instance, we could create an alias for the type `void`, or `int`, or
-    even `std::string`:
-  */
-
-  using z = void;
-  using w = int;
-  using k = std::string;
-
-  /*
-    A `int` or `std::string` runtime variable is able to store values, yes, but
-    as for the types themselves, `int` and `std::string`, they don't really
-    represent any values at compile time. Assigning values to them only make
-    sense at runtime.
-  */
-
-  PRINT << "z = " << type_str<z>();
-  PRINT << "w = " << type_str<w>();
-  PRINT << "k = " << type_str<k>();
-
-  /*
-    The last thing to notice is that types are immutable. Once we assign a type
-    to an alias, that alias will always represent the same type.
-
-    That's a hint that metaprogramming in C++ works similarly to pure
-    functional programming.
-  */
-
-  // ERROR: it's illegal to re-assign a type to an alias. Uncomment the next
-  // line for a compilation error.
-  //w = long;
-
-  /*
-    SUMMARY: metaprogramming deals with types, not values. We can simulate
-    values using types like `std::integral_constant` or `std::ratio`. But,
-    ultimately, they're just types.
-
-    Types are immutable.
-
-    Metaprograms run at compile time, whereas regular programs run at runtime.
-  */
+  COMMENT(
+    "SUMMARY: metaprogramming deals with types, not values. We can simulate "
+    "values using types like `std::integral_constant` or `std::ratio`. But, "
+    "ultimately, they're just types."
+    "\n\n"
+    "Types are immutable."
+    "\n\n"
+    "Metaprograms run at compile time, whereas regular programs run at runtime."
+  );
 }
 
+// TODO: HOW TO PRINT THIS??
 /**
- * This tutorial is a follow up to the variables tutorial above.
- *
- * Declarations of classes and structures take place outside of the tutorial
- * block for reasons outside of the scope of this tutorial, but they should be
- * considered part of it nevertheless.
- *
  * @author: Marcelo Juchem <marcelo@fb.com>
  */
+LESSON(
+  "variables, part 2/2",
+  "This lesson is a follow up to the variables lesson above."
+  "\n\n"
+  "Declarations of types below take place outside of the lesson block for "
+  "reasons outside of the scope of this guid, but they should be considered "
+  "part of it nevertheless.",
 
-struct m {
-  int value;
-};
+  struct m {
+    int value;
+  };
 
-class n {
-  void method();
-};
+  class n {
+    void method();
+  };
 
-union u {
-  int i;
-  double fp;
-};
+  union u {
+    int i;
+    double fp;
+  };
 
-enum e { field0, field1, field2 };
+  enum e { field0, field1, field2 };
 
-enum class c { field3, field4, field5 };
+  enum class c { field3, field4, field5 };
+) {
+  COMMENT(
+    "Not only type aliases, but classes, structures, enumerations and unions "
+    "can also be considered analogous to procedural programming's variables:"
+  );
 
-FATAL_TUTORIAL(variables, part 2/2) {
-  /*
-    Not only type aliases, but classes, structures, enumerations and unions can
-    also be considered analogous to procedural programming's variables:
-  */
+  PRINT("m = ", type_str<m>());
+  PRINT("n = ", type_str<n>());
+  PRINT("u = ", type_str<u>());
+  PRINT("e = ", type_str<e>());
+  PRINT("c = ", type_str<c>());
 
-  PRINT << "m = " << type_str<m>();
-  PRINT << "n = " << type_str<n>();
-  PRINT << "u = " << type_str<u>();
-  PRINT << "e = " << type_str<e>();
-  PRINT << "c = " << type_str<c>();
-  NEW_LINE;
+  COMMENT(
+    "And they can be assigned to aliases as well."
+  );
 
-  /*
-    And they can be assigned to aliases as well.
-  */
+  CODE(
+    using vm = m;
+    using vn = m;
+    using vu = m;
+    using ve = m;
+    using vc = m;
+  );
 
-  using vm = m;
-  using vn = m;
-  using vu = m;
-  using ve = m;
-  using vc = m;
-
-  PRINT << "vm = " << type_str<vm>();
-  PRINT << "vn = " << type_str<vn>();
-  PRINT << "vu = " << type_str<vu>();
-  PRINT << "ve = " << type_str<ve>();
-  PRINT << "vc = " << type_str<vc>();
+  PRINT("vm = ", type_str<vm>());
+  PRINT("vn = ", type_str<vn>());
+  PRINT("vu = ", type_str<vu>());
+  PRINT("ve = ", type_str<ve>());
+  PRINT("vc = ", type_str<vc>());
 }
 
-} // namespace tutorial {
+} // namespace lesson {
--- demo/tutorial/1_1-values.cpp
+++ demo/tutorial/1_1-values.cpp
@@ -7,268 +7,279 @@
  *  of patent rights can be found in the PATENTS file in the same directory.
  */
 
-#include <fatal/type/list.h>
+#include <fatal/lesson/driver.h>
 
-#include "./driver.h"
-
-namespace tutorial {
+namespace lesson {
 
 /**
- * This tutorial gives an overview on how values are represented. Subsequent
- * tutorials will elaborate on proper ways of achieving such representation.
- *
- * The goal, for now, is to come up with the intuition behind it without drowing
- * in syntax and correctness.
- *
  * @author: Marcelo Juchem <marcelo@fb.com>
  */
+LESSON(
+  "representing values, part 1/4",
+  "This lesson gives an overview on how values are represented. Subsequent "
+  "tutorials will elaborate on proper ways of achieving such representation."
+  "\n\n"
+  "The goal, for now, is to come up with the intuition behind it without "
+  "drowing in syntax and correctness.",
 
-template <int Value>
-struct int_constant {
-  static int value;
-};
+  template <int Value>
+  struct int_constant {
+    static int value;
+  };
 
-template <int Value>
-int int_constant<Value>::value = Value;
+  template <int Value>
+  int int_constant<Value>::value = Value;
+) {
+  COMMENT(
+    "A previous lesson mentioned that values can be emulated using types to "
+    "represent them. Here's an overview on the intuition of how this can be "
+    "achieved."
+  );
 
-FATAL_TUTORIAL(representing values, part 1/4) {
-  /*
-    A previous tutorial mentioned that values can be emulated using types to
-    represent them. Here's an overview on the intuition of how this can be
-    achieved.
-  */
-  using x = int_constant<15>;
+  CODE(
+    using x = int_constant<15>;
+  );
 
-  PRINT << "x = " << type_str<x>();
-  PRINT << "x::value = " << x::value;
-  NEW_LINE;
+  PRINT("x = ", type_str<x>());
+  PRINT("x::value = ", x::value);
 
-  /*
-    Note, however, that `int_constant::value` is a regular runtime variable
-    as opposed to a compile time constant. It is possible, for instance, to
-    change the value associated with it:
-  */
-  x::value = 30;
-  PRINT << "x::value = " << x::value;
+  COMMENT(
+    "Note, however, that `int_constant::value` is a regular runtime variable "
+    "as opposed to a compile time constant. It is possible, for instance, to "
+    "change the value associated with it:"
+  );
 
-  /*
-    This makes it illegal to use such variable as an argument to a template.
-    Template parameters must be immutable and available at compile time. This
-    includes, for instance, type and integer constants.
-  */
+  CODE(
+    x::value = 30;
+  );
 
-  // ERROR: `int_constant::value` is not a constant. Uncomment the next line for
-  // a compilation error.
-  //using y = int_constant<x::value>;
+  PRINT("x::value = ", x::value);
+
+  COMMENT(
+    "This makes it illegal to use such variable as an argument to a template. "
+    "Template parameters must be immutable and available at compile time. This "
+    "includes, for instance, type and integer constants."
+  );
+
+  ILLEGAL(
+    "`int_constant::value` is not a constant",
+    using y = int_constant<x::value>;
+  );
 }
 
 /**
- * This tutorial demonstrates proper ways to represent values that can be used
- * at compile time.
- *
- * Let's modify the `int_constant` template to properly represent compile time
- * constants.
- *
  * @author: Marcelo Juchem <marcelo@fb.com>
  */
+LESSON(
+  "representing values, part 2/4",
+  "This lesson demonstrates proper ways to represent values that can be used "
+  "at compile time."
+  "\n\n"
+  "Let's modify the `int_constant` template to properly represent compile time "
+  "constants.",
 
-template <int Value>
-struct int_constant_proper {
-  static constexpr int const value = Value;
-};
+  template <int Value>
+  struct int_constant_proper {
+    static constexpr int const value = Value;
+  };
 
-template <int Value>
-constexpr int const int_constant_proper<Value>::value;
+  template <int Value>
+  constexpr int const int_constant_proper<Value>::value;
+) {
+  COMMENT(
+    "C++11 introduced the `constexpr` keyword which roughly allows us to tell "
+    "the compiler that a given variable holds the result of a constant "
+    "expression."
+    "\n\n"
+    "Once we have such guarantee, the compiler can evaluate the contents of "
+    "such variable at compile time, effectivelly making it a compile time "
+    "constant."
+  );
 
-FATAL_TUTORIAL(representing values, part 2/4) {
-  /*
-    C++11 introduced the `constexpr` keyword which roughly allows us to tell the
-    compiler that a given variable holds the result of a constant expression.
+  CODE(
+    using x = int_constant_proper<15>;
+  );
 
-    Once we have such guarantee, the compiler can evaluate the contents of such
-    variable at compile time, effectivelly making it a compile time constant.
-  */
-  using x = int_constant_proper<15>;
+  PRINT("x = ", type_str<x>());
+  PRINT("x::value = ", x::value);
 
-  PRINT << "x = " << type_str<x>();
-  PRINT << "x::value = " << x::value;
-  NEW_LINE;
+  COMMENT(
+    "As noted before, constants can be used as template parameters."
+  );
 
-  /*
-    As noted before, constants can be used as template parameters.
-  */
-  using y = int_constant_proper<x::value>;
+  CODE(
+    using y = int_constant_proper<x::value>;
+  );
 
-  PRINT << "y = " << type_str<y>();
-  PRINT << "y::value = " << y::value;
-  NEW_LINE;
+  PRINT("y = ", type_str<y>());
+  PRINT("y::value = ", y::value);
 
-  /*
-    In fact, any expression that can be evaluated at compile time can be used as
-    a compile time constant:
-  */
+  COMMENT(
+    "In fact, any expression that can be evaluated at compile time can be used "
+    "as a compile time constant:"
+  );
 
-  using z = int_constant_proper<x::value * 2>;
+  CODE(
+    using z = int_constant_proper<x::value * 2>;
+  );
 
-  PRINT << "z = " << type_str<z>();
-  PRINT << "z::value = " << z::value;
-  NEW_LINE;
+  PRINT("z = ", type_str<z>());
+  PRINT("z::value = ", z::value);
 
-  using w = int_constant_proper<x::value + z::value - 3>;
+  CODE(
+    using w = int_constant_proper<x::value + z::value - 3>;
+  );
 
-  PRINT << "w = " << type_str<w>();
-  PRINT << "w::value = " << w::value;
+  PRINT("w = ", type_str<w>());
+  PRINT("w::value = ", w::value);
 }
 
 /**
- * This tutorial gives an overview on the implementation of
- * `std::integral_constant`.
- *
- * So far we've been limited to `int` constants. One could be interested in
- * employing other types for a constant, like `char` or `unsigned long`.
- *
- * Let's modify the `int_constant_proper` template to represent arbitrary
- * integral types.
- *
  * @author: Marcelo Juchem <marcelo@fb.com>
  */
+LESSON(
+  "representing values, part 3/4",
+  "This lesson gives an overview on the implementation of "
+  "`std::integral_constant`."
+  "\n\n"
+  "So far we've been limited to `int` constants. One could be interested in "
+  "employing other types for a constant, like `char` or `unsigned long`."
+  "\n\n"
+  "Let's modify the `int_constant_proper` template to represent arbitrary "
+  "integral types.",
 
-template <typename T, T Value>
-struct constant {
-  static constexpr T const value = Value;
-};
+  template <typename T, T Value>
+  struct constant {
+    static constexpr T const value = Value;
+  };
 
-template <typename T, T Value>
-constexpr T const constant<T, Value>::value;
-
-FATAL_TUTORIAL(representing values, part 3/4) {
-  /*
-    Now we can specify the type of the constant, as well as its value.
-  */
+  template <typename T, T Value>
+  constexpr T const constant<T, Value>::value;
+) {
+  COMMENT(
+    "Now we can specify the type of the constant, as well as its value."
+  );
   using x = constant<int, -15>;
 
-  PRINT << "x = " << type_str<x>();
-  PRINT << "x::value = " << x::value;
-  NEW_LINE;
+  PRINT("x = ", type_str<x>());
+  PRINT("x::value = ", x::value);
 
   using y = constant<bool, true>;
 
-  PRINT << "y = " << type_str<y>();
-  PRINT << "y::value = " << std::boolalpha << y::value;
-  NEW_LINE;
+  PRINT("y = ", type_str<y>());
+  PRINT("y::value = ", std::boolalpha, y::value);
 
-  /*
-    Again, any expression that can be evaluated at compile time will do:
-  */
+  COMMENT(
+    "Again, any expression that can be evaluated at compile time will do:"
+  );
 
   using z = constant<unsigned, (x::value > 0) ? x::value : -x::value>;
 
-  PRINT << "z = " << type_str<z>();
-  PRINT << "z::value = " << z::value;
+  PRINT("z = ", type_str<z>());
+  PRINT("z::value = ", z::value);
 }
 
 /**
- * This tutorial gives an overview of some basic features that
- * `std::integral_constant` offers.
- *
- * The implementation and library features built around `std::integral_constant`
- * are a bit more involved than what we've seen so far, but for the purposes of
- * a tutorial, we don't need to dig too deep.
- *
- * For now, let's look at a few more things that `std::integral_constant`offers.
- *
  * @author: Marcelo Juchem <marcelo@fb.com>
  */
-FATAL_TUTORIAL(representing values, part 4/4) {
-  /*
-    We already covered how to represent a compile time constant with a type, and
-    how to access the constant's value.
-  */
+LESSON(
+  "representing values, part 4/4",
+  "This lesson gives an overview of some basic features that "
+  "`std::integral_constant` offers."
+  "\n\n"
+  "The implementation and library features built around "
+  "`std::integral_constant` are a bit more involved than what we've seen so "
+  "far, but for the purposes of a lesson, we don't need to dig too deep."
+  "\n\n"
+  "For now, let's look at a few more things that `std::integral_constant` "
+  "offers."
+) {
+  COMMENT(
+    "We already covered how to represent a compile time constant with a type, "
+    "and how to access the constant's value."
+  );
   using x = std::integral_constant<int, -15>;
 
-  PRINT << "x = " << type_str<x>();
-  PRINT << "x::value = " << x::value;
+  PRINT("x = ", type_str<x>());
+  PRINT("x::value = ", x::value);
 
-  /*
-    For convenience purposes, `std::integral_constant` also provides an identity
-    alias in the form of a member called `type`:
-  */
-  PRINT << "x::type = " << type_str<x::type>();
+  COMMENT(
+    "For convenience purposes, `std::integral_constant` also provides an "
+    "identity alias in the form of a member called `type`:"
+  );
+  PRINT("x::type = ", type_str<x::type>());
 
-  /*
-    It also exposes the type of the constant it represents:
-  */
-  PRINT << "x::value_type = " << type_str<x::value_type>();
-  NEW_LINE;
+  COMMENT(
+    "It also exposes the type of the constant it represents:"
+  );
+  PRINT("x::value_type = ", type_str<x::value_type>());
 
-  /*
-    Shortcuts to boolean constants are also provided:
-  */
+  COMMENT(
+    "Shortcuts to boolean constants are also provided:"
+  );
 
   using t = std::true_type;
-  PRINT << "t = " << type_str<t>();
-  PRINT << "t::value = " << std::boolalpha << t::value;
-  PRINT << "t::value_type = " << type_str<t::value_type>();
-  NEW_LINE;
+  PRINT("t = ", type_str<t>());
+  PRINT("t::value = ", std::boolalpha, t::value);
+  PRINT("t::value_type = ", type_str<t::value_type>());
 
   using f = std::false_type;
-  PRINT << "f = " << type_str<f>();
-  PRINT << "f::value = " << std::boolalpha << f::value;
-  PRINT << "f::value_type = " << type_str<f::value_type>();
+  PRINT("f = ", type_str<f>());
+  PRINT("f::value = ", std::boolalpha, f::value);
+  PRINT("f::value_type = ", type_str<f::value_type>());
 }
 
 /**
- * This tutorial gives an overview on how to reduce verbosity through the use of
- * convenience aliases.
- *
- * Some types will be extensively used throughout the examples in this tutorial.
- * For instance, `std::integral_constant` for `int` values.
- *
- * For this reason, let's see how we can shorten the code we write when
- * declaring an integral constant through the use of aliases.
- *
  * @author: Marcelo Juchem <marcelo@fb.com>
  */
+LESSON(
+  "convenience aliases",
+  "This lesson gives an overview on how to reduce verbosity through the use of "
+  "convenience aliases."
+  "\n\n"
+  "Some types will be extensively used throughout the examples in this lesson. "
+  "For instance, `std::integral_constant` for `int` values."
+  "\n\n"
+  "For this reason, let's see how we can shorten the code we write when "
+  "declaring an integral constant through the use of aliases.",
 
-template <int Value>
-using int_value = std::integral_constant<int, Value>;
-
-FATAL_TUTORIAL(convenience aliases) {
-  /*
-    Let's start by going the verbose route and fully specifying `x` as an
-    `std::integral_constant`.
-  */
+  template <int Value>
+  using int_value = std::integral_constant<int, Value>;
+) {
+  COMMENT(
+    "Let's start by going the verbose route and fully specifying `x` as an "
+    "`std::integral_constant`."
+  );
 
   using x = std::integral_constant<int, 10>;
 
-  PRINT << "x = " << type_str<x>();
-  PRINT << "x::value = " << x::value;
-  NEW_LINE;
+  PRINT("x = ", type_str<x>());
+  PRINT("x::value = ", x::value);
 
-  /*
-    Now let's use the convenient alias `int_value` to declare the same thing.
-  */
+  COMMENT(
+    "Now let's use the convenient alias `int_value` to declare the same thing."
+  );
 
   using y = int_value<10>;
 
-  PRINT << "y = " << type_str<y>();
-  PRINT << "y::value = " << y::value;
+  PRINT("y = ", type_str<y>());
+  PRINT("y::value = ", y::value);
 
-  /*
-    The beauty of aliases is that they don't create new types. Instead, they're
-    just shortcuts to existing types. For instance, by checking the output of
-    this tutorial, it's easy to see that both `x` and `y` reference exactly the
-    same type: `std::integral_constant<int, 10>`.
-
-    The code below will be further explained in a later tutorial. For now, it
-    suffices to know that it will prevent the program from compiling correctly
-    if both `x` and `y` do not represent the same type.
-
-    This means that, if the line below doesn't result in a compilation error,
-    then both `x` and `y` are guaranteed to reference the same type.
-  */
+  COMMENT(
+    "The beauty of aliases is that they don't create new types. Instead, "
+    "they're just shortcuts to existing types. For instance, by checking the "
+    "output of this lesson, it's easy to see that both `x` and `y` reference "
+    "exactly the same type: `std::integral_constant<int, 10>`."
+    "\n\n"
+    "The code below will be further explained in a later lesson. For now, it "
+    "suffices to know that it will prevent the program from compiling "
+    "correctly if both `x` and `y` do not represent the same type."
+    "\n\n"
+    "This means that, if the line below doesn't result in a compilation error, "
+    "then both `x` and `y` are guaranteed to reference the same type."
+  );
 
   static_assert(std::is_same<x, y>::value, "type mismatch");
 }
 
-} // namespace tutorial {
+} // namespace lesson {
