63e4295c6fabb9b31eef8dbeb54f36e12d955213
Marcelo Juchem <marcelo@fb.com>
2015-08-05 16:50:16 -0700

    - reorganizing sources and greatly improving output of lessons

---
+++ fatal/lesson/driver.h
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_lesson_driver_h
+#define FATAL_INCLUDE_fatal_lesson_driver_h
+
+#include <fatal/lesson/lesson.h>
+
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using fatal::type_str;
+
+int main() {
+  fatal::lesson::registry::run();
+  return 0;
+}
+
+#endif // FATAL_INCLUDE_fatal_lesson_driver_h
