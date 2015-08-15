df04540e28f3d6d18e50901429e766e930a34a7d
Marcelo Juchem <marcelo@fb.com>
2015-08-14 17:20:13 -0700

    - adding statistical_moments

---
+++ fatal/math/statistical_moments.h
@@ -0,0 +1,337 @@
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_math_numerics_h
+#define FATAL_INCLUDE_fatal_math_numerics_h
+
+#include <fatal/type/traits.h>
+
+#include <utility>
+
+#include <cmath>
+
+namespace fatal {
+
+/**
+ * Online calculation of statistical moments:
+ * - mean
+ * - variance
+ * - standard deviation
+ * - skewness
+ * - kurtosis
+ *
+ * It allows, for instance, calculating these moments from a stream of samples,
+ * without the need to hold all samples in memory at the same time.
+ *
+ * Results can be queried on the fly without
+ *
+ * The arithmetic has been worked out so as to eliminate numerical errors,
+ * and make computation as close to lossless as possible.
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+template <typename T = double>
+struct statistical_moments {
+  /**
+   * The type of the samples.
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  using value_type = T;
+
+  /**
+   * The type representing the number of samples seen at any given moment.
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  using size_type = std::size_t;
+
+  /**
+   * Constructors.
+   */
+  statistical_moments() = default;
+  statistical_moments(statistical_moments const &rhs) = default;
+  statistical_moments(statistical_moments &&rhs) = default;
+
+  /**
+   * Adds a new sample from the stream.
+   *
+   * Example:
+   *
+   *  statistical_moments<> moments;
+   *
+   *  for (decltype(moments)::value_type sample; std::cin >> sample; ) {
+   *    moments.add(sample);
+   *  }
+   *
+   *  std::cout << "samples observed: " << moments.size()
+   *    << " mean: " << moments.mean()
+   *    << " variance: " << moments.variance()
+   *    << std::endl;
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  void add(fast_pass<value_type> sample) {
+    auto const n = samples_++;
+
+    auto const delta = sample - moment_1_;
+    auto const normalized_delta = delta / samples_;
+    auto const normalized_delta_squared = normalized_delta * normalized_delta;
+
+    auto const term1 = delta * normalized_delta * n;
+
+    moment_4_ += term1 * normalized_delta_squared
+      * (samples_ * samples_ - 3 * samples_ + 3)
+      + 6 * normalized_delta_squared * moment_2_
+      - 4 * normalized_delta * moment_3_;
+
+    moment_3_ += term1 * normalized_delta * (n - 1)
+      - 3 * normalized_delta * moment_2_;
+
+    moment_2_ += term1;
+
+    moment_1_ += normalized_delta;
+  }
+
+  /**
+   * Returns the mean of the samples added so far.
+   *
+   * See: http://en.wikipedia.org/wiki/Arithmetic_mean
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  fast_pass<value_type> mean() const { return moment_1_; }
+
+  /**
+   * Calculates the variance of the samples added so far.
+   *
+   * See: http://en.wikipedia.org/wiki/Variance
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  value_type variance() const {
+    return samples_ < 2 ? 0 : moment_2_ / (samples_ - 1);
+  }
+
+  /**
+   * Calculates the standard deviation of the samples added so far.
+   *
+   * This is slower than working with the variance since this function calls
+   * `std::sqrt`.
+   *
+   * See: http://en.wikipedia.org/wiki/Standard_deviation
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  value_type standard_deviation() const { return std::sqrt(variance()); }
+
+  /**
+   * Calculates the skewness of the samples added so far.
+   *
+   * This function calls `std::sqrt` and `std::pow`.
+   *
+   * See: http://en.wikipedia.org/wiki/Skewness
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  value_type skewness() const {
+    return std::sqrt(static_cast<value_type>(samples_)) * moment_3_
+      / std::pow(moment_2_, 1.5);
+  }
+
+  /**
+   * Calculates the kurtosis of the samples added so far.
+   *
+   * See: http://en.wikipedia.org/wiki/Kurtosis
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  value_type kurtosis() const {
+    return (samples_ * moment_4_) / (moment_2_ * moment_2_) - 3;
+  }
+
+  /**
+   * Tells how many samples have been added so far.
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  fast_pass<size_type> size() const { return samples_; }
+
+  /**
+   * True if no sample has been added so far, false otherwise.
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  bool empty() const { return !samples_; }
+
+  /**
+   * Clears the internal state as if no sample had been added so far.
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  void clear() {
+    samples_ = 0;
+    moment_1_ = 0;
+    moment_2_ = 0;
+    moment_3_ = 0;
+    moment_4_ = 0;
+  }
+
+  bool operator != (statistical_moments const &rhs) const {
+    return !(*this == rhs);
+  }
+
+  bool operator == (statistical_moments const &rhs) const {
+    return rhs.samples_ == samples_
+      && rhs.moment_1_ == moment_1_
+      && rhs.moment_2_ == moment_2_
+      && rhs.moment_3_ == moment_3_
+      && rhs.moment_4_ == moment_4_;
+  }
+
+  /**
+   * Merges the samples from the given instance `rhs` into this one.
+   *
+   * Example:
+   *
+   *  template <typename T, typename Stream>
+   *  void gather(statistical_moments<T> &moments, Stream &&in) {
+   *    for (T sample; in >> sample; ) {
+   *      moments.add(sample);
+   *    }
+   *  }
+   *
+   *  template <typename T>
+   *  void print(statistical_moments<T> &moments, char const *text) {
+   *    std::cout << text << " - samples observed: " << moments.size()
+   *      << " mean: " << moments.mean()
+   *      << " variance: " << moments.variance()
+   *      << std::endl;
+   *  }
+   *
+   *  int main() {
+   *    statistical_moments<> moments1;
+   *    statistical_moments<> moments2;
+   *
+   *    gather(moments1, std::cin);
+   *    gather(moments2, std::ifstream("/tmp/samples"));
+   *
+   *    print("<stdin>", moments1);
+   *    print("/tmp/samples", moments2);
+   *    print("both", moments1.merge(moments2));
+   *  }
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  statistical_moments &merge(fast_pass<statistical_moments> rhs) {
+    auto samples = samples_ + rhs.samples_;
+
+    auto const delta_1 = rhs.moment_1_ - moment_1_;
+    auto const delta_2 = delta_1 * delta_1;
+    auto const delta_3 = delta_1 * delta_2;
+    auto const delta_4 = delta_2 * delta_2;
+
+    auto moment_1 = (
+      samples_ * moment_1_ + rhs.samples_ * rhs.moment_1_
+    ) / samples;
+
+    auto moment_2 = moment_2_ + rhs.moment_2_
+      + delta_2 * samples_ * rhs.samples_ / samples;
+
+    auto moment_3 = (
+      moment_3_ + rhs.moment_3_
+        + delta_3 * samples_ * rhs.samples_ * (samples_ - rhs.samples_)
+        / (samples * samples)
+      ) + (
+        3 * delta_1 * (
+          samples_ * rhs.moment_2_ - rhs.samples_ * moment_2_
+        ) / samples
+      );
+
+    moment_4_ = (
+      moment_4_ + rhs.moment_4_
+        + delta_4 * samples_ * rhs.samples_ * (
+          samples_ * samples_
+            - samples_ * rhs.samples_
+            + rhs.samples_ * rhs.samples_
+        ) / (samples * samples * samples)
+      ) + (
+        6 * delta_2 * (
+          samples_ * samples_ * rhs.moment_2_
+            + rhs.samples_ * rhs.samples_ * moment_2_
+        ) / (samples * samples)
+        + 4 * delta_1 * (
+          samples_ * rhs.moment_3_ - rhs.samples_ * moment_3_
+        ) / samples
+      );
+
+    samples_ = samples;
+    moment_1_ = std::move(moment_1);
+    moment_2_ = std::move(moment_2);
+    moment_3_ = std::move(moment_3);
+
+    return *this;
+  }
+
+  /**
+   * A representation of the internal state.
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  using internal_state = std::tuple<
+    size_type,
+    value_type,
+    value_type,
+    value_type,
+    value_type
+  >;
+
+  /**
+   * Gets the representation of the internal state.
+   *
+   * Useful for serialization.
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  internal_state state() const {
+    return internal_state(
+      samples_,
+      moment_1_,
+      moment_2_,
+      moment_3_,
+      moment_4_
+    );
+  }
+
+  /**
+   * Constructs an instance by restoring the given internal state.
+   *
+   * Useful for deserialization.
+   *
+   * @author: Marcelo Juchem <marcelo@fb.com>
+   */
+  explicit statistical_moments(fast_pass<internal_state> state):
+    samples_(std::get<0>(state)),
+    moment_1_(std::get<1>(state)),
+    moment_2_(std::get<2>(state)),
+    moment_3_(std::get<3>(state)),
+    moment_4_(std::get<4>(state))
+  {}
+
+private:
+  size_type samples_ = 0;
+  value_type moment_1_ = 0;
+  value_type moment_2_ = 0;
+  value_type moment_3_ = 0;
+  value_type moment_4_ = 0;
+};
+
+} // namespace fatal {
+
+#endif // FATAL_INCLUDE_fatal_math_numerics_h
---
+++ fatal/math/test/statistical_moments_test.cpp
@@ -0,0 +1,303 @@
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <fatal/math/statistical_moments.h>
+#include <fatal/type/scalar.h>
+
+#include <fatal/test/driver.h>
+
+#include <random>
+#include <ratio>
+#include <vector>
+
+namespace fatal {
+
+using value_type = long double;
+
+// number of iterations for some tests
+using iterations = size_constant<100>;
+
+// how many rounds to run on each randomized test
+using rounds = size_constant<100>;
+
+// the number of samples for each randomized test
+using samples = size_constant<100000>;
+
+// the relative error allowed when comparing results
+using relative_error = std::ratio<1, 100>;
+
+// the absolute error to use when comparing results
+using absolute_error = std::ratio<1, 1000>;
+
+// the precision to use when comparing results that should be equal
+using equality_precision = std::ratio<1, 10000000>;
+
+////////////////////
+// TEST UTILITIES //
+////////////////////
+
+template <typename T, typename TRNG, typename TDistribution>
+std::vector<T> random_samples(
+  std::size_t samples,
+  TRNG &&rng,
+  TDistribution &&distribution
+) {
+  std::vector<T> result;
+  result.reserve(samples);
+
+  while (samples--) {
+    result.push_back(distribution(rng));
+  }
+
+  return result;
+}
+
+template <typename T, typename TRNG, typename TDistribution>
+void statistical_moments_test_round(
+  std::size_t const samples,
+  TRNG &&rng,
+  TDistribution &&distribution
+) {
+  using value_type = T;
+
+  FATAL_ASSERT_GT(samples, 0);
+
+  auto const v1 = random_samples<value_type>(samples / 2, rng, distribution);
+  auto const v2 = random_samples<value_type>(
+    samples / 2 + (samples & 1),
+    rng,
+    distribution
+  );
+  FATAL_ASSERT_EQ(samples, v1.size() + v2.size());
+
+  statistical_moments<value_type> moments; // all samples
+  statistical_moments<value_type> moments1; // samples in v1
+  statistical_moments<value_type> moments2; // samples in v2
+
+  FATAL_EXPECT_TRUE(moments.empty());
+  FATAL_EXPECT_TRUE(moments1.empty());
+  FATAL_EXPECT_TRUE(moments2.empty());
+
+  // calculates the moments using the streaming algorithm
+
+  value_type sum = 0;
+
+  for (auto i: v1) {
+    sum += i;
+    moments.add(i);
+    moments1.add(i);
+  }
+
+  for (auto i: v2) {
+    sum += i;
+    moments.add(i);
+    moments2.add(i);
+  }
+
+  FATAL_EXPECT_FALSE(moments.empty());
+  FATAL_EXPECT_FALSE(moments1.empty());
+  FATAL_EXPECT_FALSE(moments2.empty());
+
+  FATAL_EXPECT_EQ(samples, moments.size());
+  FATAL_EXPECT_EQ(v1.size(), moments1.size());
+  FATAL_EXPECT_EQ(v2.size(), moments2.size());
+
+  // calculates the moments using the traditional approach, for later comparison
+
+  auto const mean = sum / samples;
+
+  value_type cumulant_1 = 0;
+  value_type cumulant_3 = 0;
+  value_type cumulant_4 = 0;
+
+  for (auto i: v1) {
+    auto const x = i - mean;
+    cumulant_1 += x * x;
+    cumulant_3 += x * x * x;
+    cumulant_4 += x * x * x * x;
+  }
+
+  for (auto i: v2) {
+    auto const x = i - mean;
+    cumulant_1 += x * x;
+    cumulant_3 += x * x * x;
+    cumulant_4 += x * x * x * x;
+  }
+
+  auto const variance = cumulant_1 / samples;
+  auto const standard_deviation = std::sqrt(variance);
+  auto const kurtosis = cumulant_4 / samples / (variance * variance) - 3;
+  auto const skewness = cumulant_3 / samples / (variance * standard_deviation);
+
+  // checks the results
+
+// checks if results are different by no more than precision
+# define TEST_ABSOLUTE_ERROR_IMPL( \
+  expected, \
+  actual, \
+  relative_error, \
+  absolute_error \
+) \
+  do { \
+    FATAL_ASSERT_GE(absolute_error, 0); \
+    FATAL_EXPECT_LE(expected - absolute_error, actual); \
+    FATAL_EXPECT_GE(expected + absolute_error, actual); \
+  } while (false)
+
+// checks if results are different by no more than threshold percent,
+// or precision if such percentage of the expected result is smaller
+// than the precision
+# define TEST_RELATIVE_ERROR_IMPL( \
+  expected, \
+  actual, \
+  relative_error, \
+  absolute_error \
+) \
+  do { \
+    auto const margin = std::abs(expected * relative_error); \
+    \
+    if (margin < absolute_error) { \
+      TEST_ABSOLUTE_ERROR_IMPL( \
+        expected, \
+        actual, \
+        relative_error, \
+        absolute_error \
+      ); \
+    } else { \
+      FATAL_EXPECT_LE(expected - margin, actual); \
+      FATAL_EXPECT_GE(expected + margin, actual); \
+    } \
+  } while (false)
+
+# define TEST_ALL_IMPL(Fn, ...) \
+  do { \
+    Fn(TEST_RELATIVE_ERROR_IMPL, mean, __VA_ARGS__); \
+    Fn(TEST_RELATIVE_ERROR_IMPL, variance, __VA_ARGS__); \
+    Fn(TEST_RELATIVE_ERROR_IMPL, standard_deviation, __VA_ARGS__); \
+    Fn(TEST_ABSOLUTE_ERROR_IMPL, skewness, __VA_ARGS__); \
+    Fn(TEST_ABSOLUTE_ERROR_IMPL, kurtosis, __VA_ARGS__); \
+  } while (false)
+
+# define TEST_IMPL(Fn, what, subject) \
+  Fn( \
+    what, \
+    subject.what(), \
+    (to_scalar<relative_error, long double>()), \
+    (to_scalar<absolute_error, long double>()) \
+  )
+
+  // compares `moments` against the expected results
+  TEST_ALL_IMPL(TEST_IMPL, moments);
+
+# undef TEST_IMPL
+# define TEST_IMPL(Fn, what, expected, actual) \
+  Fn( \
+    expected.what(), \
+    actual.what(), \
+    (to_scalar<relative_error, long double>()), \
+    (to_scalar<equality_precision, long double>()) \
+  )
+
+  // tests merging into an empty instance
+  statistical_moments<value_type> merge_into_empty;
+  merge_into_empty.merge(moments);
+  TEST_ALL_IMPL(TEST_IMPL, moments, merge_into_empty);
+
+  // tests merging two sets of samples
+  auto merged_subsets(moments1);
+  merged_subsets.merge(moments2);
+  TEST_ALL_IMPL(TEST_IMPL, moments, merged_subsets);
+
+# undef TEST_IMPL
+# undef TEST_ALL_IMPL
+}
+
+template <typename T, typename TRNG, typename TDistribution>
+void test_statistical_moments(
+  std::size_t const samples,
+  TRNG &&rng,
+  TDistribution &&distribution
+) {
+  for (auto i = rounds::value; i--; ) {
+    statistical_moments_test_round<T>(samples, rng, distribution);
+  }
+}
+
+///////////
+// TESTS //
+///////////
+
+// the minimum value of a sample on uniform distribution randomized tests
+using uniform_min = std::ratio<-1000>;
+
+// the maximum value of a sample on uniform distribution randomized tests
+using uniform_max = std::ratio<1000>;
+
+static_assert(std::ratio_less_equal<uniform_min, uniform_max>::value, "");
+
+FATAL_TEST(statistical_moments, uniform_distribution) {
+  test_statistical_moments<value_type>(
+    samples::value,
+    random_data(),
+    std::uniform_real_distribution<value_type>(
+      to_scalar<uniform_min, long double>(),
+      to_scalar<uniform_max, long double>()
+    )
+  );
+}
+
+// the mean to use for normal distribution randomized tests
+using normal_mean = std::ratio<0>;
+
+// the standard deviation to use for normal distribution randomized tests
+using normal_stddev = std::ratio<10>;
+
+FATAL_TEST(statistical_moments, normal_distribution) {
+  test_statistical_moments<value_type>(
+    samples::value,
+    random_data(),
+    std::normal_distribution<value_type>(
+      to_scalar<normal_mean, long double>(),
+      to_scalar<normal_stddev, long double>()
+    )
+  );
+}
+
+// the rate (lambda) to use for exponential distribution randomized tests
+using exponential_lambda = std::ratio<10>;
+
+FATAL_TEST(statistical_moments, exponential_distribution) {
+  test_statistical_moments<value_type>(
+    samples::value,
+    random_data(),
+    std::exponential_distribution<value_type>(
+      to_scalar<exponential_lambda, long double>()
+    )
+  );
+}
+
+FATAL_TEST(statistical_moments, state) {
+  random_data rng;
+  std::normal_distribution<value_type> distribution(
+    to_scalar<normal_mean, long double>(),
+    to_scalar<normal_stddev, long double>()
+  );
+
+  statistical_moments<value_type> moments;
+  statistical_moments<value_type> empty_copy(moments.state());
+  FATAL_EXPECT_EQ(moments, empty_copy);
+
+  for (auto i = iterations::value; i--; ) {
+    moments.add(distribution(rng));
+    statistical_moments<value_type> copy(moments.state());
+    FATAL_EXPECT_EQ(moments, copy);
+  }
+}
+
+} // namespace fatal {
