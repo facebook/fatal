9c2a52f3fe020606b726c305cec752b7c5035f1a
Marcelo Juchem <marcelo@fb.com>
2015-08-26 15:35:22 -0700

    - not skipping contbuild when compiler is missing

--- contbuild.sh
+++ contbuild.sh
@@ -9,13 +9,13 @@ if [ -z "$cc" ]; then
   exit 1
 fi
 
-if which $cc; then
-  echo "compiler: $cc"
-  $cc --version
-  # TODO: split prefix_tree_benchmark into several benchmarks to avoid
-  #       breaking contbuild and remove the `nobuild` flag, then remove
-  #       this for loop and let `validate.sh` figure the compilers out
-  USE_CC=$cc NO_CLEAR=true ./validate.sh nobuild
-else
-  echo "no $cc installed, skipping validation"
-fi
+if ! which $cc; then
+  echo "no $cc installed"
+  exit 1
+
+echo "compiler: $cc"
+$cc --version
+# TODO: split prefix_tree_benchmark into several benchmarks to avoid
+#       breaking contbuild and remove the `nobuild` flag, then remove
+#       this for loop and let `validate.sh` figure the compilers out
+USE_CC=$cc NO_CLEAR=true ./validate.sh nobuild
