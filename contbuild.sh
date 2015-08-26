75c6820d2bc1358b73d162d8edcae2eab688c1d5
Marcelo Juchem <marcelo@fb.com>
2015-08-26 15:31:00 -0700

    - preventing contbuild from building with more compilers than each setting requires

--- contbuild.sh
+++ contbuild.sh
@@ -2,32 +2,20 @@
 
 set -e
 
-for cc in g++-4.8 g++-4.9 g++-5 clang-3.4 clang-3.5 clang-3.6 clang-3.7 clang-3.8; do
-  if which $cc; then
-    echo "Package: $cc"
-    dpkg -s $cc | grep '^Status: '
-  fi
-done
+cc="$1"
 
-for cc in g++-4.8 g++-4.9 g++-5; do
-  if which $cc; then
-    $cc --version
-    # TODO: split prefix_tree_benchmark into several benchmarks to avoid
-    #       breaking contbuild and remove the `nobuild` flag, then remove
-    #       this for loop and let `validate.sh` figure the compilers out
-    USE_CC=$cc NO_CLEAR=true ./validate.sh nobuild
-  else
-    echo "no $cc installed, skipping validation"
-  fi
-done
+if [ -z "$cc" ]; then
+  echo "Usage: $0 compiler"
+  exit 1
+fi
 
-for cc in clang++-3.8 clang++-3.4 clang++ clang++-3.5 clang++-3.6 clang++-3.7; do
-  if which $cc; then
-    $cc --version
-    # TODO: split prefix_tree_benchmark into several benchmarks to avoid
-    #       breaking contbuild and remove the `nobuild` flag
-    USE_CC=$cc NO_CLEAR=true ./validate.sh nobuild
-  else
-    echo "no $cc installed, skipping validation"
-  fi
-done
+if which $cc; then
+  echo "compiler: $cc"
+  $cc --version
+  # TODO: split prefix_tree_benchmark into several benchmarks to avoid
+  #       breaking contbuild and remove the `nobuild` flag, then remove
+  #       this for loop and let `validate.sh` figure the compilers out
+  USE_CC=$cc NO_CLEAR=true ./validate.sh nobuild
+else
+  echo "no $cc installed, skipping validation"
+fi
