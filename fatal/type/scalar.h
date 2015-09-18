bf7ea31c88b72c4ae63eea9f88bd65cdea677b68
Marcelo Juchem <marcelo@fb.com>
2015-09-18 15:16:25 -0700

    - adding documentation and tests to scalar.h

--- fatal/type/scalar.h
+++ fatal/type/scalar.h
@@ -17,15 +17,28 @@
 
 namespace fatal {
 
-// TODO: DOCUMENT AND TEST
+/**
+ * A shortcut to `std::integral_constant` for `bool`.
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 template <bool Value>
 using bool_constant = std::integral_constant<bool, Value>;
 
-// TODO: DOCUMENT AND TEST
+/**
+ * A shortcut to `std::integral_constant` for `std::size_t`.
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 template <std::size_t Value>
 using size_constant = std::integral_constant<std::size_t, Value>;
 
-// TODO: DOCUMENT AND TEST
+/**
+ * Given a type `T`, returns a `std::integral_constant` of type `std::size_t`
+ * whose value is `sizeof(T)`.
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 template <typename T>
 using size_of_constant = std::integral_constant<std::size_t, sizeof(T)>;
 
@@ -35,7 +48,25 @@ template <typename> struct to_scalar;
 } // namespace scalar_impl {
 } // namespace detail {
 
-// TODO: DOCUMENT AND TEST
+/**
+ * Gets a scalar representation from the given type.
+ *
+ * Example:
+ *
+ *  // yields `10` with type `int`
+ *  auto result1 = to_scalar<std::integral_constant<int, 10>>();
+ *
+ *  // yields `10` with type `long`
+ *  auto result2 = to_scalar<std::integral_constant<int, 10>, long>();
+ *
+ *  // yields `5.6` with type `double`
+ *  auto result3 = to_scalar<std::ratio<56, 10>>();
+ *
+ *  // yields `5.6` with type `float`
+ *  auto result4 = to_scalar<std::ratio<56, 10>, float>();
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 template <
   typename From,
   typename T = typename detail::scalar_impl::to_scalar<From>::default_type
@@ -58,7 +89,27 @@ struct to_integral_impl {
 } // namespace scalar_impl {
 } // namespace detail {
 
-// TODO: DOCUMENT AND TEST
+/**
+ * Converts any integral to its appropriate integer value.
+ *
+ * This is particularly useful when dealing with type-safe enumerations, or
+ * when dealing with both integers and enumerations in a transparent manner.
+ *
+ * Example:
+ *
+ *  // yields `10` with type `int`
+ *  auto result1 = to_integral(10);
+ *
+ *  enum class E: char { a = 2, b = 4 };
+ *
+ *  // yields `2` with type `char`
+ *  auto result2 = to_integral(E::a);
+ *
+ *  // yields `6` with type `char`
+ *  auto result3 = to_integral(E::a) | to_integral(E::b);
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 template <typename T>
 typename detail::scalar_impl::to_integral_impl<T>::type to_integral(
   T const value
@@ -66,37 +117,110 @@ typename detail::scalar_impl::to_integral_impl<T>::type to_integral(
   return detail::scalar_impl::to_integral_impl<T>::convert(value);
 }
 
-// TODO: DOCUMENT AND TEST
+/**
+ * Merges the bits of all integral parameters. This is equivalent to a bitwise
+ * OR of all the parameters.
+ *
+ * This is particularly useful when dealing with type-safe enumerations.
+ *
+ * Example:
+ *
+ *  // yields `11`
+ *  auto result1 = bitwise_merge(1, 2, 8);
+ *
+ *  enum class E: char { a = 1, b = 2, c = 4 };
+ *
+ *  // yields `7` with type `E`
+ *  auto result2 = bitwise_merge(E::a, E::b, E::c);
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 template <typename T>
-T bitwise_merge(T const value, T const head) {
-  return static_cast<T>(to_integral(value) | to_integral(head));
-}
+T bitwise_merge(T const value) { return value; }
 
-// TODO: DOCUMENT AND TEST
 template <typename T, typename... Args>
-T bitwise_merge(T const value, T const head, Args... args) {
-  return bitwise_merge(bitwise_merge(value, head), args...);
-}
-
-// TODO: DOCUMENT AND TEST
-template <typename T, typename... Args>
-T bitwise_filter(T const value, Args... args) {
-  return static_cast<T>(
-    to_integral(value) & to_integral(bitwise_merge(args...))
+T bitwise_merge(T const lhs, T const rhs, Args... args) {
+  return bitwise_merge(
+    static_cast<T>(to_integral(lhs) | to_integral(rhs)),
+    args...
   );
 }
 
-// TODO: DOCUMENT AND TEST
+/**
+ * Returns an intersection of the bits of all parameters. This is equivalent to
+ * a bitwise AND of all the parameters.
+ *
+ * This is particularly useful when dealing with type-safe enumerations.
+ *
+ * Example:
+ *
+ *  // yields `2`
+ *  auto result1 = bitwise_filter(6, 2, 10);
+ *
+ *  enum class E: char { a = 1, b = 2, c = 4 };
+ *
+ *  // yields `0` with type `E`
+ *  auto result2 = bitwise_filter(E::a, E::b, E::c);
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+template <typename T>
+T bitwise_filter(T const value) { return value; }
+
+template <typename T, typename... Args>
+T bitwise_filter(T const lhs, T const rhs, Args... args) {
+  return bitwise_filter(
+    static_cast<T>(to_integral(lhs) & to_integral(rhs)),
+    args...
+  );
+}
+
+/**
+ * Tells whether `value` contains all the bits set in every subsequent argument.
+ *
+ * Example:
+ *
+ *  // yields `true`
+ *  auto result1 = bitwise_has_all(7, 1, 2, 4);
+ *
+ *  // yields `false`
+ *  auto result2 = bitwise_has_all(6, 1, 2, 4);
+ *
+ *  enum class E: char { a = 1, b = 2, c = 4 };
+ *
+ *  // yields `false`
+ *  auto result3 = bitwise_has_all(bitwise_merge(E::a, E::c), E::a, E::b, E::c);
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 template <typename T, typename... Args>
 bool bitwise_has_all(T const value, Args... args) {
   auto const merged = bitwise_merge(args...);
-  return static_cast<T>(to_integral(value) & to_integral(merged)) == merged;
+  return static_cast<T>(bitwise_filter(value, merged)) == merged;
 }
 
-// TODO: DOCUMENT AND TEST
+/**
+ * Tells whether `value` contains at least one of the bits set in at least one
+ * of the subsequent arguments.
+ *
+ * Example:
+ *
+ *  // yields `true`
+ *  auto result1 = bitwise_has_any(6, 1, 2, 4);
+ *
+ *  // yields `false`
+ *  auto result2 = bitwise_has_any(8, 1, 2, 4);
+ *
+ *  enum class E: char { a = 1, b = 2, c = 4 };
+ *
+ *  // yields `true`
+ *  auto result3 = bitwise_has_any(bitwise_merge(E::a, E::c), E::a, E::b, E::c);
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
 template <typename T, typename... Args>
 bool bitwise_has_any(T const value, Args... args) {
-  return to_integral(bitwise_filter(value, args...)) != 0;
+  return to_integral(bitwise_filter(value, bitwise_merge(args...))) != 0;
 }
 
 ////////////////////////////
