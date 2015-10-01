6587d26f66237b713f0ed45d250ac881dbb6a846
Marcelo Juchem <marcelo@fb.com>
2015-10-01 15:39:07 -0700

    - enabling separate contbuilds for c++11, c++14 and c++1z

--- contbuild.sh
+++ contbuild.sh
@@ -3,7 +3,6 @@
 set -e
 
 cc="$1"
-
 if [ -z "$cc" ]; then
   echo "Usage: $0 compiler"
   exit 1
@@ -14,14 +13,17 @@ if ! which $cc; then
   exit 1
 fi
 
+cpp_std="$2"
+
 #if [ "$cc" = "g++-4.8" ]; then
 #  USE_CC=$cc NO_CLEAR=true PRE_PROC=true ./test.sh \
 #    type reflect_member_function || true
 #fi
 
 echo "compiler: $cc"
+echo "standard: $cpp_std"
 $cc --version
 # TODO: split prefix_tree_benchmark into several benchmarks to avoid
 #       breaking contbuild and remove the `nobuild` flag, then remove
 #       this for loop and let `validate.sh` figure the compilers out
-USE_CC=$cc NO_CLEAR=true ./validate.sh nobuild
+USE_CC="$cc" USE_STD="$cpp_std" NO_CLEAR=true ./validate.sh nobuild
