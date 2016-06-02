6e9c19dc9650caa1e4c303405af4b198fefa8f03
Marcelo Juchem <marcelo@fb.com>
2016-06-02 11:35:32 -0700

    - adding script to help with running a single test on all supported compilers

---
+++ full_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. ./scripts.inc
+
+set -e
+
+lclear
+
+for cc in $full_compilers_list; do
+  for std in `flag_std_cpp11 $cc` `flag_std_cpp14 $cc` `flag_std_cpp17 $cc`; do
+    NO_CLEAR=true USE_CC="$cc" USE_STD="$std" ./test.sh "$@"
+  done
+done
