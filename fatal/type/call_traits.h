84ce63fda85321fc06164827ad318b622241370e
Marcelo Juchem <marcelo@fb.com>
2016-07-16 12:14:01 -0700

    - updating copyright year

--- fatal/type/call_traits.h
+++ fatal/type/call_traits.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) 2016, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -12,7 +12,7 @@
 
 #include <fatal/preprocessor.h>
 #include <fatal/type/sequence.h>
-#include <fatal/type/transform.h>
+#include <fatal/type/deprecated/transform.h>
 
 #include <utility>
 
