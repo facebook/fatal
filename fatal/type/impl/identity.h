bf20bff4415c67bedc0ff7dfe1a70090a8cc1f36
Marcelo Juchem <marcelo@fb.com>
2016-07-16 12:27:35 -0700

    - adding leaner identity transform implementation

---
+++ fatal/type/impl/identity.h
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_type_impl_identity_h
+#define FATAL_INCLUDE_fatal_type_impl_identity_h
+
+namespace fatal {
+namespace impl_identity {
+
+template <typename T>
+struct identity {
+  using type = T;
+};
+
+} // namespace impl_identity {
+} // namespace fatal {
+
+#endif // FATAL_INCLUDE_fatal_type_impl_identity_h
