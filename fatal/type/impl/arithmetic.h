c3e224086e4c81454a08294d5f53c0e9e0941cbf
Marcelo Juchem <marcelo@fb.com>
2016-07-16 12:26:56 -0700

    - adding leaner arithmetic transform implementation

---
+++ fatal/type/impl/arithmetic.h
@@ -0,0 +1,91 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_type_impl_arithmetic_h
+#define FATAL_INCLUDE_fatal_type_impl_arithmetic_h
+
+#include <type_traits>
+
+namespace fatal {
+namespace impl_arithmetic {
+
+// TODO: do it in logarithmic time
+template <typename T, typename... Args>
+struct add<T, Args...> {
+  using type = std::integral_constant<
+    decltype(T::value + add<Args...>::type::value),
+    (T::value + add<Args...>::type::value)
+  >;
+};
+
+template <typename T>
+struct add<T> {
+  using type = T;
+};
+
+template <typename LHS, typename RHS, typename... Args>
+struct sub<LHS, RHS, Args...> {
+  using type = typename sub<
+    typename sub<LHS, RHS>::type,
+    Args...
+  >::type;
+};
+
+template <typename LHS, typename RHS>
+struct sub<LHS, RHS> {
+  using type = std::integral_constant<
+    decltype(LHS::value - RHS::value),
+    (LHS::value - RHS::value)
+  >;
+};
+
+// TODO: do it in logarithmic time
+template <typename T, typename... Args>
+struct mul<T, Args...> {
+  using type = std::integral_constant<
+    decltype(T::value * mul<Args...>::type::value),
+    (T::value * mul<Args...>::type::value)
+  >;
+};
+
+template <typename T>
+struct mul<T> {
+  using type = T;
+};
+
+template <typename LHS, typename RHS, typename... Args>
+struct div<LHS, RHS, Args...> {
+  using type = typename div<typename div<LHS, RHS>::type, Args...>::type;
+};
+
+template <typename LHS, typename RHS>
+struct div<LHS, RHS> {
+  using type = std::integral_constant<
+    decltype(LHS::value / RHS::value),
+    (LHS::value / RHS::value)
+  >;
+};
+
+template <typename LHS, typename RHS, typename... Args>
+struct mod<LHS, RHS, Args...> {
+  using type = typename mod<typename mod<LHS, RHS>::type, Args...>::type;
+};
+
+template <typename LHS, typename RHS>
+struct mod<LHS, RHS> {
+  using type = std::integral_constant<
+    decltype(LHS::value % RHS::value),
+    (LHS::value % RHS::value)
+  >;
+};
+
+} // namespace impl_arithmetic {
+} // namespace fatal {
+
+#endif // FATAL_INCLUDE_fatal_type_impl_arithmetic_h
