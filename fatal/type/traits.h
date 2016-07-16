dd40603f266e9ec36ccfdac02e6015c3f5d40d8a
Marcelo Juchem <marcelo@fb.com>
2016-07-16 12:18:19 -0700

    - updating copyright year

--- fatal/type/traits.h
+++ fatal/type/traits.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) 2016, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -14,7 +14,7 @@
 #include <fatal/type/fast_pass.h> // TODO: REMOVE AND FIX DEPENDENCIES
 #include <fatal/type/scalar.h>
 #include <fatal/type/sequence.h>
-#include <fatal/type/transform.h>
+#include <fatal/type/deprecated/transform.h>
 
 #include <type_traits>
 #include <utility>
