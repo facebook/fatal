c495d0215e46f33930344d9422d78549fb6afb7d
Marcelo Juchem <marcelo@fb.com>
2016-07-16 12:06:33 -0700

    - moving identity out of transform.h

---
+++ fatal/type/identity.h
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_type_identity_h
+#define FATAL_INCLUDE_fatal_type_identity_h
+
+#include <fatal/type/impl/identity.h>
+
+namespace fatal {
+
+/**
+ * Helper alias similar to std::decay_t, that resolves to the type
+ * given as the template parameter.
+ *
+ * Example:
+ *
+ *  // yields `int`
+ *  using result1 = identity<int>;
+ *
+ *  // yields `std::string`
+ *  using result2 = identity<std::string>;
+ *
+ *  // yields `double`
+ *  using result3 = identity<identity<double>>>;
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+template <typename T>
+using identity = typename impl_identity::identity<T>::type;
+
+} // namespace fatal {
+
+#endif // FATAL_INCLUDE_fatal_type_identity_h
