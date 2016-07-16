427c8663970d24c08dcf0809a947ebb92c85637c
Marcelo Juchem <marcelo@fb.com>
2016-07-16 12:32:52 -0700

    - adding leaner `transform` implementation

---
+++ fatal/type/impl/transform.h
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_type_impl_transform_h
+#define FATAL_INCLUDE_fatal_type_impl_transform_h
+
+#include <fatal/type/at.h>
+#include <fatal/type/pair.h>
+
+namespace fatal {
+namespace impl_transform {
+
+template <typename> struct transform;
+
+template <template <typename...> class T, typename... Args>
+struct transform<T<Args...>> {
+  template <template <typename> class Transform>
+  using apply = T<Transform<Args>...>;
+};
+
+template <typename> struct map_transform;
+
+template <template <typename...> class T, typename... Args>
+struct map_transform<T<Args...>> {
+  template <
+    template <typename> class KeyTransform,
+    template <typename> class ValueTransform
+  >
+  using apply = T<
+    pair<
+      KeyTransform<first<Args>>,
+      ValueTransform<second<Args>>
+    >...
+  >;
+};
+
+} // namespace impl_transform {
+} // namespace fatal {
+
+#endif // FATAL_INCLUDE_fatal_type_impl_transform_h
--- fatal/type/transform.h
+++ fatal/type/transform.h
@@ -10,6 +10,26 @@
 #ifndef FATAL_INCLUDE_fatal_type_transform_h
 #define FATAL_INCLUDE_fatal_type_transform_h
 
+#include <fatal/type/identity.h>
+
+#include <fatal/type/impl/transform.h>
+
+namespace fatal {
+
+template <typename T, template <typename> class Transform>
+using transform = typename impl_transform::transform<T>
+  ::template apply<Transform>;
+
+template <
+  typename T,
+  template <typename> class KeyTransform,
+  template <typename> class ValueTransform = identity
+>
+using map_transform = typename impl_transform::map_transform<T>
+  ::template apply<KeyTransform, ValueTransform>;
+
+} // namespace fatal {
+
 #include <fatal/type/deprecated/transform.h>
 
 #endif // FATAL_INCLUDE_fatal_type_transform_h
