6ef3821decaa29ee018444c4e26de73343ce77c2
Marcelo Juchem <marcelo@fb.com>
2016-07-18 15:03:36 -0700

    - adding benchmarks for `head`

---
+++ fatal/type/benchmark/head/slice.cpp
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <fatal/benchmark/cases.h>
+#include <fatal/type/list.h>
+#include <fatal/type/slice.h>
+
+template <int> struct foo {};
+
+int main() {
+  using namespace fatal;
+
+# define INNER(Index) \
+  prevent_optimization(slice<lst, 0, Index>())
+
+# define WRAP(Value) foo<Value>
+# define OUTER(...) \
+  do { \
+    using lst = list<__VA_ARGS__>; \
+    FATAL_BENCHMARK_CASES_MONOTONIC_INNER_0_255(INNER); \
+  } while (false)
+
+  FATAL_BENCHMARK_CASES_CSV_OUTER_0_255(OUTER, WRAP);
+
+  return 0;
+}
