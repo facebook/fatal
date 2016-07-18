1fa910a2fdffb373b42ea498eeb27baf4962f0b5
Marcelo Juchem <marcelo@fb.com>
2016-07-18 11:27:14 -0700

    - adding compile time benchmarks for `list` and `type_list`

---
+++ fatal/type/benchmark/list/list.cpp
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <fatal/benchmark/cases.h>
+#include <fatal/type/list.h>
+
+template <int> struct foo {};
+
+int main() {
+  using namespace fatal;
+
+# define WRAP(Value) foo<Value>
+# define OUTER(...) \
+    do { \
+      prevent_optimization(list<__VA_ARGS__>()); \
+    } while (false)
+
+  FATAL_BENCHMARK_CASES_CSV_OUTER_0_1000(OUTER, WRAP);
+
+  return 0;
+}
