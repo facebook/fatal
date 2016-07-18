9f59442b9a861e76e3fe38e8d11f9eb4a9502cc0
Marcelo Juchem <marcelo@fb.com>
2016-07-18 11:21:59 -0700

    - removing `split` from slice.h

--- fatal/type/slice.h
+++ fatal/type/slice.h
@@ -36,9 +36,6 @@ using pick = typename impl_at::pick<T, Indexes...>::type;
 template <typename T, std::size_t Begin, std::size_t End = size<T>::value>
 using slice = typename impl_at::slice<T, make_index_interval<Begin, End>>::type;
 
-template <typename T, std::size_t Offset>
-using split = slice<T, 0, Offset>;
-
 } // namespace fatal {
 
 #endif // FATAL_INCLUDE_fatal_type_slice_h
