2c39427a80a76ddb55c154aee3a2993b6f097d8b
Marcelo Juchem <marcelo@fb.com>
2016-07-26 21:07:51 -0700

    - implementing `sorted_search`

---
+++ fatal/type/impl/search.h
@@ -0,0 +1,135 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_type_impl_search_h
+#define FATAL_INCLUDE_fatal_type_impl_search_h
+
+#include <fatal/type/size.h>
+#include <fatal/type/slice.h>
+#include <fatal/type/tag.h>
+
+namespace fatal {
+namespace impl_srch {
+
+// TODO: OPTIMIZE COMPILE TIMES
+// TODO: HIGHER LOG BASE OPTIMIZATION (3 or 4 should be enough)
+// TODO: SWITCH CASE OPTIMIZATION
+
+template <typename T, std::size_t Offset = 0, std::size_t Size = size<T>::value>
+struct srt {
+  template <
+    template <typename> class Filter,
+    template <typename> class Comparer,
+    typename Needle,
+    typename Visitor,
+    typename... Args
+  >
+  static constexpr bool sorted_search(
+    Needle &&needle,
+    Visitor &&visitor,
+    Args &&...args
+  ) {
+    static_assert(Offset + (Size / 2) < size<T>::value, "");
+    using pivot = at<T, Offset + (Size / 2)>;
+    return Comparer<Filter<pivot>>::greater(needle)
+      ? srt<T, Offset, Size / 2>::template sorted_search<Filter, Comparer>(
+        std::forward<Needle>(needle),
+        std::forward<Visitor>(visitor),
+        std::forward<Args>(args)...
+      )
+      : Comparer<Filter<pivot>>::less(needle)
+        ? srt<T, (Offset + Size / 2) + 1, Size / 2 - !(Size & 1)>
+          ::template sorted_search<Filter, Comparer>(
+            std::forward<Needle>(needle),
+            std::forward<Visitor>(visitor),
+            std::forward<Args>(args)...
+          )
+        : (
+          visitor(
+            indexed<pivot, Offset + (Size / 2)>(),
+            std::forward<Args>(args)...
+          ), true
+        );
+  }
+};
+
+template <typename T, std::size_t Offset>
+struct srt<T, Offset, 0> {
+  template <
+    template <typename> class,
+    template <typename> class,
+    typename... Args
+  >
+  static constexpr bool sorted_search(Args &&...) {
+    return false;
+  }
+};
+
+template <typename T, std::size_t Offset>
+struct srt<T, Offset, 1> {
+  template <
+    template <typename> class Filter,
+    template <typename> class Comparer,
+    typename Needle,
+    typename Visitor,
+    typename... Args
+  >
+  static constexpr bool sorted_search(
+    Needle &&needle,
+    Visitor &&visitor,
+    Args &&...args
+  ) {
+    static_assert(Offset < size<T>::value,  "");
+    return Comparer<Filter<at<T, Offset>>>::equal(needle) && (
+      visitor(
+        indexed<at<T, Offset>, Offset>(),
+        std::forward<Args>(args)...
+      ),
+      true
+    );
+  }
+};
+
+template <typename T, std::size_t Offset>
+struct srt<T, Offset, 2> {
+  template <
+    template <typename> class Filter,
+    template <typename> class Comparer,
+    typename Needle,
+    typename Visitor,
+    typename... Args
+  >
+  static constexpr bool sorted_search(
+    Needle &&needle,
+    Visitor &&visitor,
+    Args &&...args
+  ) {
+    static_assert(Offset + 1 < size<T>::value,  "");
+    return (
+      Comparer<Filter<at<T, Offset>>>::equal(needle) && (
+        visitor(
+          indexed<at<T, Offset>, Offset>(),
+          std::forward<Args>(args)...
+        ), true
+      )
+    ) || (
+      Comparer<Filter<at<T, Offset + 1>>>::equal(needle) && (
+        visitor(
+          indexed<at<T, Offset + 1>, Offset + 1>(),
+          std::forward<Args>(args)...
+        ), true
+      )
+    );
+  }
+};
+
+} // namespace impl_srch {
+} // namespace fatal {
+
+#endif // FATAL_INCLUDE_fatal_type_impl_search_h
---
+++ fatal/type/search.h
@@ -0,0 +1,78 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_type_search_h
+#define FATAL_INCLUDE_fatal_type_search_h
+
+#include <fatal/functional/no_op.h>
+#include <fatal/type/identity.h>
+
+#include <utility>
+
+#include <fatal/type/impl/search.h>
+
+namespace fatal {
+
+// TODO: MOVE IT SOMEWHERE ELSE
+template <typename LHS>
+struct value_comparer {
+  template <typename RHS>
+  static constexpr bool less(RHS &&rhs) {
+    return LHS::value < rhs;
+  }
+
+  template <typename RHS>
+  static constexpr bool equal(RHS &&rhs) {
+    return LHS::value == rhs;
+  }
+
+  template <typename RHS>
+  static constexpr bool greater(RHS &&rhs) {
+    return LHS::value > rhs;
+  }
+};
+
+// TODO: USE A CUSTOM COMPARER
+// TODO: DOCUMENT THE NEED FOR A SORTED LIST
+template <
+  typename T,
+  template <typename> class Filter = identity,
+  template <typename> class Comparer = value_comparer,
+  typename Needle,
+  typename Visitor,
+  typename... Args
+>
+constexpr bool sorted_search(
+  Needle &&needle,
+  Visitor &&visitor,
+  Args &&...args
+) {
+  return impl_srch::srt<T>::template sorted_search<Filter, Comparer>(
+    std::forward<Needle>(needle),
+    std::forward<Visitor>(visitor),
+    std::forward<Args>(args)...
+  );
+}
+
+template <
+  typename T,
+  template <typename> class Filter = identity,
+  template <typename> class Comparer = value_comparer,
+  typename Needle
+>
+constexpr bool sorted_search(Needle &&needle) {
+  return sorted_search<T, Filter, Comparer>(
+    std::forward<Needle>(needle),
+    fn::no_op()
+  );
+}
+
+} // namespace fatal {
+
+#endif // FATAL_INCLUDE_fatal_type_search_h
