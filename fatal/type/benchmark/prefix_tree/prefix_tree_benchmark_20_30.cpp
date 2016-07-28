36a72be71111412e48b2038f54e7c12b9a16ca22
Marcelo Juchem <marcelo@fb.com>
2016-07-28 11:57:43 -0700

    - adding benchmarks for `prefix_tree`

---
+++ fatal/type/benchmark/prefix_tree/prefix_tree_benchmark_20_30.cpp
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <fatal/type/benchmark/prefix_tree/prefix_tree.h>
+
+#include <fatal/benchmark/driver.h>
+
+namespace fatal {
+
+CREATE_BENCHMARK(n20_len30,
+  s30_00, s30_01, s30_02, s30_03, s30_04,
+  s30_05, s30_06, s30_07, s30_08, s30_09,
+  s30_10, s30_11, s30_12, s30_13, s30_14,
+  s30_15, s30_16, s30_17, s30_18, s30_19
+);
+
+} // namespace fatal {
