63e66e46f1600bd1f5251a43a6936f3890aa26a5
Marcelo Juchem <marcelo@fb.com>
2016-08-08 23:07:19 -0700

    - adding compile time benchmark for `partition`

---
+++ fatal/type/benchmark/partition/sequence.cpp
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <fatal/benchmark/cases.h>
+#include <fatal/type/sequence.h>
+#include <fatal/type/sort.h>
+
+struct flt {
+  template <typename T, T Value>
+  using apply = std::integral_constant<bool, Value % 2 == 0>;
+};
+
+int main() {
+  using namespace fatal;
+
+# define OUTER(...) \
+  do { \
+    prevent_optimization(partition<sequence<int, __VA_ARGS__>, flt>()); \
+  } while (false)
+
+  FATAL_BENCHMARK_CASES_CSV_OUTER_0_1000_SHUFFLED(OUTER);
+
+  return 0;
+}
