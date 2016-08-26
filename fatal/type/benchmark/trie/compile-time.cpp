bd7fb7ffca69bce50d925e07231d3dbf3383cbea
Marcelo Juchem <marcelo@fb.com>
2016-08-25 18:42:29 -0700

    - adding compile-time benchmarks for `trie`

---
+++ fatal/type/benchmark/trie/compile-time.cpp
@@ -0,0 +1,20 @@
+#include <fatal/test/words.h>
+#include <fatal/type/trie.h>
+
+#include <iostream>
+#include <string>
+
+int main() {
+  using namespace fatal;
+
+  for (std::string needle; std::cin >> needle; ) {
+    std::cout << needle << ": " << std::boolalpha
+      << trie_find<random_500_words<list, sequence>>(
+        needle.begin(),
+        needle.end()
+      )
+      << std::endl;
+  }
+
+  return 0;
+}
