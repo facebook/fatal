8bc8f09549dd20b5abdf4af42f180ad51a39fb5c
Marcelo Juchem <marcelo@fb.com>
2016-09-06 17:52:31 -0700

    - adding `get_type::{fault|faults}`

--- fatal/type/get_type.h
+++ fatal/type/get_type.h
@@ -94,6 +94,8 @@ namespace get_type {
   FATAL_IMPL_GET_TYPE_TYPE(enums);
   FATAL_IMPL_GET_TYPE_TYPE(extension);
   FATAL_IMPL_GET_TYPE_TYPE(extensions);
+  FATAL_IMPL_GET_TYPE_TYPE(fault);
+  FATAL_IMPL_GET_TYPE_TYPE(faults);
   FATAL_IMPL_GET_TYPE_TYPE(first);
   FATAL_IMPL_GET_TYPE_TYPE(flag);
   FATAL_IMPL_GET_TYPE_TYPE(flags);
