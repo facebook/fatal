1b0a09659608da621a96c28c08da8f776334cb43
Marcelo Juchem <marcelo@fb.com>
2016-09-28 18:45:40 -0700

    - adding `demo` directory to `smoke_build.sh`

--- smoke_build.sh
+++ smoke_build.sh
@@ -16,6 +16,11 @@ for cc in $full_compilers_list; do
   for std in `flag_std_cpp11 $cc` `flag_std_cpp14 $cc` `flag_std_cpp17 $cc`; do
     echo "## full build $cc $std"
     USE_CC="$cc" USE_STD="$std" USE_CCACHE=true NO_CLEAR=true ./build.sh
+    if [ "$std" != "`flag_std_cpp11 $cc`" ]; then
+      for f in `find demo -name '*.cpp'`; do
+        USE_CC="$cc" USE_STD="$std" USE_CCACHE=true NO_CLEAR=true ./compile.sh "$f"
+      done
+    fi
     echo
     echo
   done
