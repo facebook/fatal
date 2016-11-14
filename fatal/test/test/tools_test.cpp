fb4a91f41387c7596288fdd50be5ef1973becfe4
Marcelo Juchem <marcelo@fb.com>
2016-11-14 14:44:58 -0800

    - adding tools to output binary data for tests

---
+++ fatal/test/test/tools_test.cpp
@@ -0,0 +1,82 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <fatal/test/tools.h>
+
+#include <fatal/test/driver.h>
+
+namespace fatal {
+namespace test {
+
+#define TEST_IMPL(Expected, Actual) \
+  do { \
+    auto const expected = (Expected); \
+    auto const actual = (Actual); \
+    if (expected != actual) { \
+      std::cout << "expected: " << expected << '\n'; \
+      std::cout << "  actual: " << actual << '\n'; \
+    } \
+    FATAL_EXPECT_EQ(expected, actual); \
+  } while (false)
+
+FATAL_TEST(to_string_literal, expanded) {
+  TEST_IMPL(R"("")", as_literal<literal_style::expanded>(""));
+  TEST_IMPL(R"("\x00")", as_literal<literal_style::expanded>(std::string("", 1)));
+  TEST_IMPL(
+    R"("\x68\x65\x6c\x6c\x6f\x2c\x20\x77\x6f\x72\x6c\x64")",
+    as_literal<literal_style::expanded>("hello, world")
+  );
+  TEST_IMPL(
+    R"("\x10\x20\x30")",
+    as_literal<literal_style::expanded>("\x10\x20\x30")
+  );
+  TEST_IMPL(
+    R"("\x0a\x14\x1e")",
+    as_literal<literal_style::expanded>("\xa\x14\x1e")
+  );
+  TEST_IMPL(
+    R"("\x0a\x0d\x09\x0b\x07\x08\x0c\xff\x3b\xab\x61\x05)"
+    R"(\x20\x09\x27\x22\x13\x10\x0d\x0a")",
+    as_literal<literal_style::expanded>(
+      "\n\r\t\v\a\b\f\xff;\xab""a\x5 \x9'\"\x13""\x10""\xd""\xa"
+    )
+  );
+}
+
+FATAL_TEST(to_string_literal, readable) {
+  TEST_IMPL("", as_literal<literal_style::readable>(""));
+  TEST_IMPL("00", as_literal<literal_style::readable>(std::string("", 1)));
+  TEST_IMPL(
+    "68 65 6c 6c   6f 2c 20 77   6f 72 6c 64",
+    as_literal<literal_style::readable>("hello, world")
+  );
+  TEST_IMPL("10 20 30", as_literal<literal_style::readable>("\x10\x20\x30"));
+  TEST_IMPL("0a 14 1e", as_literal<literal_style::readable>("\xa\x14\x1e"));
+  TEST_IMPL(
+    "0a 0d 09 0b   07 08 0c ff   3b ab 61 05   20 09 27 22   13 10 0d 0a",
+    as_literal<literal_style::readable>(
+      "\n\r\t\v\a\b\f\xff;\xab""a\x5 \x9'\"\x13""\x10""\xd""\xa"
+    )
+  );
+}
+
+FATAL_TEST(to_string_literal, compact) {
+  TEST_IMPL(R"("")", as_literal(""));
+  TEST_IMPL(R"("\0")", as_literal(std::string("", 1)));
+  TEST_IMPL(R"("hello, world")", as_literal("hello, world"));
+  TEST_IMPL(R"("\x10 0")", as_literal("\x10\x20\x30"));
+  TEST_IMPL(R"("\n\x14\x1e")", as_literal("\xa\x14\x1e"));
+  TEST_IMPL(
+    R"("\n\r\t\v\a\b\f\xff;\xab""a\x5 \t'\"\x13\x10\r\n")",
+    as_literal("\n\r\t\v\a\b\f\xff;\xab""a\x5 \x9'\"\x13""\x10""\xd""\xa")
+  );
+}
+
+} // namespace test {
+} // namespace fatal {
---
+++ fatal/test/tools.h
@@ -0,0 +1,124 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_test_tools_h
+#define FATAL_INCLUDE_fatal_test_tools_h
+
+#include <sstream>
+#include <string>
+
+#include <cstdint>
+
+namespace fatal {
+namespace test {
+
+inline char lshd(std::uint8_t c) {
+  return "0123456789abcdef"[c & 0xf];
+}
+
+enum class literal_style { compact, expanded, readable };
+
+template <
+  literal_style Style = literal_style::compact,
+  typename Out,
+  typename Buffer
+>
+void to_string_literal(Out &out, Buffer const &v) {
+  switch (Style) {
+    case literal_style::expanded: {
+      out << '"';
+      for (std::uint8_t c: v) {
+        out << "\\x";
+        out << lshd(c >> 4);
+        out << lshd(c);
+      }
+      out << '"';
+      break;
+    }
+    case literal_style::readable: {
+      std::size_t size = 0;
+      for (std::uint8_t c: v) {
+        if (size) {
+          out << ' ';
+          if (size % 4 == 0) {
+            out << "  ";
+          }
+        }
+        out << lshd(c >> 4);
+        out << lshd(c);
+        ++size;
+      }
+      break;
+    }
+    case literal_style::compact: {
+      out << '"';
+      bool hex = false;
+      for (std::uint8_t c: v) {
+        if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f')
+          || (c >= 'A' && c <= 'F')
+        ) {
+          if (hex) {
+            out << "\"\"";
+            out << c;
+            hex = false;
+          } else {
+            out << c;
+          }
+        } else if (c >= 32 && c < 127) {
+          hex = false;
+          switch (c) {
+            case '\\': case '"':
+              out << '\\';
+            default:
+              out << c;
+          }
+        } else {
+          hex = false;
+          switch (c) {
+            case '\0': out << "\\0"; break;
+            case '\n': out << "\\n"; break;
+            case '\r': out << "\\r"; break;
+            case '\t': out << "\\t"; break;
+            case '\v': out << "\\v"; break;
+            case '\a': out << "\\a"; break;
+            case '\b': out << "\\b"; break;
+            case '\f': out << "\\f"; break;
+            default:
+              out << "\\x";
+              if (c >> 4) {
+                out << lshd(c >> 4);
+              }
+              out << lshd(c);
+              //out << std::hex << static_cast<std::uint8_t>(c);
+              hex = true;
+          }
+        }
+      }
+      out << '"';
+      break;
+    }
+  }
+}
+
+template <literal_style Style = literal_style::compact, typename Buffer>
+std::string to_string_literal(Buffer const &v) {
+  std::ostringstream ss;
+  to_string_literal<Style>(ss, v);
+  return ss.str();
+}
+
+template <literal_style Style = literal_style::compact>
+std::string as_literal(std::string const &s) {
+  return to_string_literal<Style>(s);
+}
+
+} // namespace test {
+} // namespace fatal {
+
+#endif // FATAL_INCLUDE_fatal_test_tools_h
