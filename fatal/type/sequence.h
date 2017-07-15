d3aa3b4b70e54a0322fe58325f974e51fdb23b94
Yedidya Feldblum <yfeldblum@fb.com>
2017-07-15 06:18:14 -0700

    Revert making sequence an alias
    Summary:
    [Fatal] Revert making `sequence` an alias.
    
    We observe incredible regressions in build speed and memory usage by gcc in some edge cases, which blame to {D5384706} (39320f32e7c43af7bd409f5320f3410a743c78c0). In one extreme case, `cc1plus` takes 20x the memory as before.
    
    After some quick experiments, this appears to blame to `sequence` being an alias at all, rather than the aliased type. So, let us revert the part of the blamed chamed which makes `sequence` into an alias.
    
    Reviewed By: luciang, philippv
    
    Differential Revision: D5431025
    
    fbshipit-source-id: a2cfba3b8a371769ee6a17089f601b3d1d3f483e

--- fatal/type/sequence.h
+++ fatal/type/sequence.h
@@ -10,17 +10,9 @@
 #ifndef FATAL_INCLUDE_fatal_type_sequence_h
 #define FATAL_INCLUDE_fatal_type_sequence_h
 
-#include <utility>
-
 #include <fatal/type/debug.h>
 
-#if __cpp_lib_integer_sequence >= 201304 || _MSC_VER
-#define FATAL_SEQUENCE_HAS_STD_INTEGER_SEQUENCE 1
-#else
-#define FATAL_SEQUENCE_HAS_STD_INTEGER_SEQUENCE 0
-#endif
-
-#if FATAL_SEQUENCE_HAS_STD_INTEGER_SEQUENCE && ( \
+#if ( \
       (__clang_major__ >= 3 && __clang_minor__ >= 8) || \
       _MSC_FULL_VER >= 190023918 || \
       0 \
@@ -32,17 +24,8 @@
 
 namespace fatal {
 
-#if FATAL_SEQUENCE_HAS_STD_INTEGER_SEQUENCE
-
-template <typename T, T... Ts>
-using sequence = std::integer_sequence<T, Ts...>;
-
-#else
-
 template <typename T, T...>
 struct sequence;
-
-#endif
 
 } // namespace fatal {
 
@@ -52,7 +35,10 @@ struct sequence;
 
 namespace fatal {
 
-#if FATAL_SEQUENCE_HAS_STD_INTEGER_SEQUENCE
+template <typename T, T... Values>
+struct sequence {
+  using value_type = T;
+};
 
 #if FATAL_SEQUENCE_HAS_INTRINSIC_MAKE_INTEGER_SEQ
 
@@ -60,18 +46,6 @@ template <typename T, std::size_t Size>
 using make_sequence = __make_integer_seq<sequence, T, Size>;
 
 #else
-
-template <typename T, std::size_t Size>
-using make_sequence = typename impl_seq::make<T, Size>::type;
-
-#endif
-
-#else
-
-template <typename T, T... Values>
-struct sequence {
-  using value_type = T;
-};
 
 template <typename T, std::size_t Size>
 using make_sequence = typename impl_seq::make<T, Size>::type;
