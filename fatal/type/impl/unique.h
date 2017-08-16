de05d0cd968887762d14f2d86f580ae65d9c79c7
Marcelo Juchem <juchem@users.noreply.github.com>
2017-08-16 16:22:22 -0700

    adding `adjacent_unique_by`
    Summary: Closes https://github.com/facebook/fatal/pull/85
    
    Reviewed By: eduardo-elizondo
    
    Differential Revision: D5620343
    
    Pulled By: yfeldblum
    
    fbshipit-source-id: c01858630535fce006c20b1417858476b4d43011

--- fatal/type/impl/unique.h
+++ fatal/type/impl/unique.h
@@ -15,6 +15,10 @@
 namespace fatal {
 namespace impl_uq {
 
+/////////////////////
+// adjacent_unique //
+/////////////////////
+
 // TODO: OPTIMIZE
 // unique recursion //
 template <template <typename...> class, typename...> struct a;
@@ -52,14 +56,99 @@ struct a<Variadic, list<Result...>, T> {
   using type = Variadic<Result..., T>;
 };
 
+////////////////////////
+// adjacent_unique_by //
+////////////////////////
+
+// unique recursion //
+template <bool, template <typename...> class, typename...> struct A;
+
+// unique //
+template <
+  template <typename...> class Variadic,
+  typename Comparer,
+  typename... Result,
+  typename LHS,
+  typename RHS,
+  typename... Args
+>
+struct A<
+  false,
+  Variadic,
+  Comparer,
+  list<Result...>,
+  LHS, RHS, Args...
+>:
+  A<
+    Comparer::template apply<LHS, RHS>::value,
+    Variadic, Comparer, list<Result..., LHS>, RHS, Args...
+  >
+{};
+
+// duplicate //
+template <
+  template <typename...> class Variadic,
+  typename Comparer,
+  typename... Result,
+  typename LHS,
+  typename RHS,
+  typename... Args
+>
+struct A<true, Variadic, Comparer, list<Result...>, LHS, RHS, Args...>:
+  A<
+    Comparer::template apply<LHS, RHS>::value,
+    Variadic, Comparer, list<Result...>, RHS, Args...
+  >
+{};
+
+// base case //
+template <
+  template <typename...> class Variadic,
+  typename Comparer,
+  typename... Result,
+  typename T
+>
+struct A<false, Variadic, Comparer, list<Result...>, T> {
+  using type = Variadic<Result..., T>;
+};
+
+// base case //
+template <
+  template <typename...> class Variadic,
+  typename Comparer,
+  typename... Result,
+  typename T
+>
+struct A<true, Variadic, Comparer, list<Result...>, T> {
+  using type = Variadic<Result...>;
+};
+
+/////////////////
+// entry_point //
+/////////////////
+
 // unique entry point //
-template <typename> struct au;
+template <typename...> struct au;
 
 template <template <typename...> class Variadic, typename... Args>
 struct au<Variadic<Args...>>: a<Variadic, list<>, Args...> {};
 
 template <template <typename...> class Variadic>
 struct au<Variadic<>> {
+  using type = Variadic<>;
+};
+
+template <
+  template <typename...> class Variadic,
+  typename... Args,
+  typename Comparer
+>
+struct au<Variadic<Args...>, Comparer>:
+  A<false, Variadic, Comparer, list<>, Args...>
+{};
+
+template <template <typename...> class Variadic, typename Comparer>
+struct au<Variadic<>, Comparer> {
   using type = Variadic<>;
 };
 
