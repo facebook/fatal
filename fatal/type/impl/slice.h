facebook/fatal

commit e030ff227a001f7265fb75e2d6422847ff34eaac

adding `index_search()`
Summary:
- adding `index_search()`
- adding tests for `search.h`
- [break] renaming `index<Container>::of<Element>` to `index<Container>::apply<Element>`
- adding `try_index` and `try_index_of`

Closes https://github.com/facebook/fatal/pull/88

Reviewed By: andrewjcg

Differential Revision: D5703254

Pulled By: yfeldblum

fbshipit-source-id: 19d63c2f927d8ddd74e2669c98bf58ae6fabe45b

fatal/type/impl/slice.h
@@ -588,14 +588,24 @@ struct h<Offset, Sequence<T, Values...>>:
 };
 
 template <typename T, std::size_t Index>
-static std::integral_constant<std::size_t, Index> itfind(indexed<T, Index>);
+static std::integral_constant<std::size_t, Index> I(indexed<T, Index>);
 
-template <typename T, T, std::size_t> struct vindexed {};
+template <std::size_t, typename T, std::size_t Index>
+static std::integral_constant<std::size_t, Index> TI(indexed<T, Index>);
+
+template <std::size_t Size, typename>
+static std::integral_constant<std::size_t, Size> TI(...);
+
+template <typename T, T, std::size_t> struct vi {};
 
 template <typename T, T Value, std::size_t Index>
-static std::integral_constant<std::size_t, Index> ivfind(
-  vindexed<T, Value, Index>
-);
+static std::integral_constant<std::size_t, Index> IV(vi<T, Value, Index>);
+
+template <std::size_t, typename T, T Value, std::size_t Index>
+static std::integral_constant<std::size_t, Index> IV(vi<T, Value, Index>);
+
+template <std::size_t Size, typename T, T>
+static std::integral_constant<std::size_t, Size> IV(...);
 
 template <typename...> struct i;
 
@@ -606,7 +616,7 @@ template <
 >
 struct i<List<Args...>, index_sequence<Indexes...>> {
   template <typename T>
-  using of = decltype(itfind<T>(inherit<indexed<Args, Indexes>...>()));
+  using apply = decltype(I<T>(inherit<indexed<Args, Indexes>...>()));
 };
 
 template <
@@ -617,8 +627,33 @@ template <
 >
 struct i<Sequence<T, Values...>, index_sequence<Indexes...>> {
   template <T Value>
-  using of = decltype(
-    ivfind<T, Value>(inherit<vindexed<T, Values, Indexes>...>())
+  using apply = decltype(IV<T, Value>(inherit<vi<T, Values, Indexes>...>()));
+};
+
+template <typename...> struct ti;
+
+template <
+  template <typename...> class List,
+  typename... Args,
+  std::size_t... Indexes
+>
+struct ti<List<Args...>, index_sequence<Indexes...>> {
+  template <typename T>
+  using apply = decltype(
+    TI<sizeof...(Args), T>(inherit<indexed<Args, Indexes>...>())
+  );
+};
+
+template <
+  template <typename V, V...> class Sequence,
+  typename T,
+  T... Values,
+  std::size_t... Indexes
+>
+struct ti<Sequence<T, Values...>, index_sequence<Indexes...>> {
+  template <T Value>
+  using apply = decltype(
+    IV<sizeof...(Values), T, Value>(inherit<vi<T, Values, Indexes>...>())
   );
 };
 

fatal/type/slice.h
@@ -73,7 +73,13 @@ template <typename T, std::size_t Begin, std::size_t End>
 using slice = head<tail<T, Begin>, End - Begin>;
 
 template <typename Container>
-using index = typename i_at::i<
+using index = i_at::i<
+  Container,
+  make_index_sequence<size<Container>::value>
+>;
+
+template <typename Container>
+using try_index = i_at::ti<
   Container,
   make_index_sequence<size<Container>::value>
 >;
@@ -104,7 +110,10 @@ struct get_third_argument {
 
 // TODO: REDUCE TEMPLATE INSTANTIATIONS
 template <typename T, typename Of>
-using index_of = typename index<T>::template of<Of>;
+using index_of = typename index<T>::template apply<Of>;
+
+template <typename T, typename Of>
+using try_index_of = typename try_index<T>::template apply<Of>;
 
 namespace bound {
 

fatal/type/search.h
@@ -93,6 +93,19 @@ static inline constexpr bool scalar_search(Needle &&needle) {
   );
 }
 
+template <typename T, typename Visitor, typename... Args>
+static inline constexpr bool index_search(
+  std::size_t needle,
+  Visitor &&visitor,
+  Args &&...args
+) {
+  return sorted_search<T, index<T>::template apply>(
+    needle,
+    std::forward<Visitor>(visitor),
+    std::forward<Args>(args)...
+  );
+}
+
 } // namespace fatal {
 
 #endif // FATAL_INCLUDE_fatal_type_search_h

fatal/type/test/sanity_check_test.cpp
@@ -676,33 +676,6 @@ void run_sanity_check() {
   );
   SAME(pair<seq, index_sequence<>>, split<seq, size<seq>::value>);
 
-  EQUAL(0, index_of<lst, int>);
-  EQUAL(1, index_of<lst, double>);
-  EQUAL(2, index_of<lst, bool>);
-  EQUAL(3, index_of<lst, void>);
-  EQUAL(4, index_of<lst, float>);
-  EQUAL(5, index_of<lst, char>);
-  EQUAL(6, index_of<lst, unsigned>);
-
-  EQUAL(0, index<lst>::of<int>);
-  EQUAL(1, index<lst>::of<double>);
-  EQUAL(2, index<lst>::of<bool>);
-  EQUAL(3, index<lst>::of<void>);
-  EQUAL(4, index<lst>::of<float>);
-  EQUAL(5, index<lst>::of<char>);
-  EQUAL(6, index<lst>::of<unsigned>);
-
-  EQUAL(0, index<shuf_sq>::of<0>);
-  EQUAL(1, index<shuf_sq>::of<8>);
-  EQUAL(2, index<shuf_sq>::of<9>);
-  EQUAL(3, index<shuf_sq>::of<6>);
-  EQUAL(4, index<shuf_sq>::of<7>);
-  EQUAL(5, index<shuf_sq>::of<5>);
-  EQUAL(6, index<shuf_sq>::of<4>);
-  EQUAL(7, index<shuf_sq>::of<1>);
-  EQUAL(8, index<shuf_sq>::of<3>);
-  EQUAL(9, index<shuf_sq>::of<2>);
-
   SAME(
     list<void *, double, bool, void, float, char, unsigned>,
     replace<lst>::at<0, void *>

fatal/type/test/search_test.cpp
@@ -1,0 +1,83 @@
+#include <fatal/type/search.h>
+
+#include <fatal/type/list.h>
+#include <fatal/type/sequence.h>
+
+#include <fatal/test/driver.h>
+#include <fatal/log/log.h>
+
+namespace fatal {
+
+template <typename Expected, std::size_t ExpectedIndex>
+struct search_visitor {
+  template <typename Actual, std::size_t Index>
+  void operator ()(indexed<Actual, Index>) const {
+    FATAL_EXPECT_SAME<Expected, Actual>();
+    FATAL_EXPECT_EQ(ExpectedIndex, Index);
+  }
+};
+
+template <std::size_t Value, std::size_t Index>
+using value_search_visitor = search_visitor<size_constant<Value>, Index>;
+
+FATAL_TEST(sorted_search, empty) {
+  using h = index_list<>;
+  FATAL_EXPECT_FALSE(sorted_search<h>(10, value_search_visitor<10, 0>()));
+  FATAL_EXPECT_FALSE(sorted_search<h>(20, value_search_visitor<20, 1>()));
+  FATAL_EXPECT_FALSE(sorted_search<h>(30, value_search_visitor<30, 2>()));
+  FATAL_EXPECT_FALSE(sorted_search<h>(40, value_search_visitor<40, 3>()));
+  FATAL_EXPECT_FALSE(sorted_search<h>(50, value_search_visitor<50, 4>()));
+  FATAL_EXPECT_FALSE(sorted_search<h>(60, value_search_visitor<60, 5>()));
+}
+
+FATAL_TEST(sorted_search, list) {
+  using h = index_list<10, 20, 30, 40, 50>;
+  FATAL_EXPECT_TRUE(sorted_search<h>(10, value_search_visitor<10, 0>()));
+  FATAL_EXPECT_TRUE(sorted_search<h>(20, value_search_visitor<20, 1>()));
+  FATAL_EXPECT_TRUE(sorted_search<h>(30, value_search_visitor<30, 2>()));
+  FATAL_EXPECT_TRUE(sorted_search<h>(40, value_search_visitor<40, 3>()));
+  FATAL_EXPECT_TRUE(sorted_search<h>(50, value_search_visitor<50, 4>()));
+  FATAL_EXPECT_FALSE(sorted_search<h>(60, value_search_visitor<60, 5>()));
+}
+
+FATAL_TEST(scalar_search, empty) {
+  using h = index_list<>;
+  FATAL_EXPECT_FALSE(scalar_search<h>(10, value_search_visitor<10, 0>()));
+  FATAL_EXPECT_FALSE(scalar_search<h>(20, value_search_visitor<20, 1>()));
+  FATAL_EXPECT_FALSE(scalar_search<h>(30, value_search_visitor<30, 2>()));
+  FATAL_EXPECT_FALSE(scalar_search<h>(40, value_search_visitor<40, 3>()));
+  FATAL_EXPECT_FALSE(scalar_search<h>(50, value_search_visitor<50, 4>()));
+  FATAL_EXPECT_FALSE(scalar_search<h>(60, value_search_visitor<60, 5>()));
+}
+
+FATAL_TEST(scalar_search, list) {
+  using h = index_list<50, 20, 10, 40, 30>;
+  FATAL_EXPECT_TRUE(scalar_search<h>(10, value_search_visitor<10, 0>()));
+  FATAL_EXPECT_TRUE(scalar_search<h>(20, value_search_visitor<20, 1>()));
+  FATAL_EXPECT_TRUE(scalar_search<h>(30, value_search_visitor<30, 2>()));
+  FATAL_EXPECT_TRUE(scalar_search<h>(40, value_search_visitor<40, 3>()));
+  FATAL_EXPECT_TRUE(scalar_search<h>(50, value_search_visitor<50, 4>()));
+  FATAL_EXPECT_FALSE(scalar_search<h>(60, value_search_visitor<60, 5>()));
+}
+
+FATAL_TEST(index_search, empty) {
+  using h = list<>;
+  FATAL_EXPECT_FALSE(index_search<h>(0, search_visitor<void, 0>()));
+  FATAL_EXPECT_FALSE(index_search<h>(1, search_visitor<bool, 1>()));
+  FATAL_EXPECT_FALSE(index_search<h>(2, search_visitor<double, 2>()));
+  FATAL_EXPECT_FALSE(index_search<h>(3, search_visitor<int, 3>()));
+  FATAL_EXPECT_FALSE(index_search<h>(4, search_visitor<unsigned, 4>()));
+  FATAL_EXPECT_FALSE(index_search<h>(5, search_visitor<void *, 5>()));
+}
+
+FATAL_TEST(index_search, list) {
+  using h = list<void, bool, double, int, unsigned>;
+  FATAL_EXPECT_TRUE(index_search<h>(0, search_visitor<void, 0>()));
+  FATAL_EXPECT_TRUE(index_search<h>(1, search_visitor<bool, 1>()));
+  FATAL_EXPECT_TRUE(index_search<h>(2, search_visitor<double, 2>()));
+  FATAL_EXPECT_TRUE(index_search<h>(3, search_visitor<int, 3>()));
+  FATAL_EXPECT_TRUE(index_search<h>(4, search_visitor<unsigned, 4>()));
+  FATAL_EXPECT_FALSE(index_search<h>(5, search_visitor<void *, 5>()));
+}
+
+} // namespace fatal {

fatal/type/test/slice_test.cpp
@@ -1,0 +1,125 @@
+#include <fatal/type/slice.h>
+
+#include <fatal/type/list.h>
+#include <fatal/type/sequence.h>
+
+#include <fatal/test/driver.h>
+#include <fatal/log/log.h>
+
+namespace fatal {
+
+FATAL_TEST(index_of, list) {
+  using ls = list<int, double, bool, void, float, char, unsigned>;
+  FATAL_EXPECT_EQ(0, index_of<ls, int>::value);
+  FATAL_EXPECT_EQ(1, index_of<ls, double>::value);
+  FATAL_EXPECT_EQ(2, index_of<ls, bool>::value);
+  FATAL_EXPECT_EQ(3, index_of<ls, void>::value);
+  FATAL_EXPECT_EQ(4, index_of<ls, float>::value);
+  FATAL_EXPECT_EQ(5, index_of<ls, char>::value);
+  FATAL_EXPECT_EQ(6, index_of<ls, unsigned>::value);
+}
+
+FATAL_TEST(index apply, list) {
+  using ls = list<int, double, bool, void, float, char, unsigned>;
+  FATAL_EXPECT_EQ(0, index<ls>::apply<int>::value);
+  FATAL_EXPECT_EQ(1, index<ls>::apply<double>::value);
+  FATAL_EXPECT_EQ(2, index<ls>::apply<bool>::value);
+  FATAL_EXPECT_EQ(3, index<ls>::apply<void>::value);
+  FATAL_EXPECT_EQ(4, index<ls>::apply<float>::value);
+  FATAL_EXPECT_EQ(5, index<ls>::apply<char>::value);
+  FATAL_EXPECT_EQ(6, index<ls>::apply<unsigned>::value);
+}
+
+FATAL_TEST(index apply, sequence) {
+  using sq = index_sequence<0, 8, 9, 6, 7, 5, 4, 1, 3, 2>;
+  FATAL_EXPECT_EQ(0, index<sq>::apply<0>::value);
+  FATAL_EXPECT_EQ(1, index<sq>::apply<8>::value);
+  FATAL_EXPECT_EQ(2, index<sq>::apply<9>::value);
+  FATAL_EXPECT_EQ(3, index<sq>::apply<6>::value);
+  FATAL_EXPECT_EQ(4, index<sq>::apply<7>::value);
+  FATAL_EXPECT_EQ(5, index<sq>::apply<5>::value);
+  FATAL_EXPECT_EQ(6, index<sq>::apply<4>::value);
+  FATAL_EXPECT_EQ(7, index<sq>::apply<1>::value);
+  FATAL_EXPECT_EQ(8, index<sq>::apply<3>::value);
+  FATAL_EXPECT_EQ(9, index<sq>::apply<2>::value);
+}
+
+FATAL_TEST(try_index_of, empty list) {
+  using ls = list<>;
+  FATAL_EXPECT_EQ(size<ls>::value, try_index_of<ls, int>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index_of<ls, double>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index_of<ls, bool>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index_of<ls, void>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index_of<ls, float>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index_of<ls, char>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index_of<ls, unsigned>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index_of<ls, void *>::value);
+}
+
+FATAL_TEST(try_index_of, list) {
+  using ls = list<int, double, bool, void, float, char, unsigned>;
+  FATAL_EXPECT_EQ(0, try_index_of<ls, int>::value);
+  FATAL_EXPECT_EQ(1, try_index_of<ls, double>::value);
+  FATAL_EXPECT_EQ(2, try_index_of<ls, bool>::value);
+  FATAL_EXPECT_EQ(3, try_index_of<ls, void>::value);
+  FATAL_EXPECT_EQ(4, try_index_of<ls, float>::value);
+  FATAL_EXPECT_EQ(5, try_index_of<ls, char>::value);
+  FATAL_EXPECT_EQ(6, try_index_of<ls, unsigned>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index_of<ls, void *>::value);
+}
+
+FATAL_TEST(try_index apply, empty list) {
+  using ls = list<>;
+  FATAL_EXPECT_EQ(size<ls>::value, try_index<ls>::apply<int>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index<ls>::apply<double>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index<ls>::apply<bool>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index<ls>::apply<void>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index<ls>::apply<float>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index<ls>::apply<char>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index<ls>::apply<unsigned>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index<ls>::apply<void *>::value);
+}
+
+FATAL_TEST(try_index apply, list) {
+  using ls = list<int, double, bool, void, float, char, unsigned>;
+  FATAL_EXPECT_EQ(0, try_index<ls>::apply<int>::value);
+  FATAL_EXPECT_EQ(1, try_index<ls>::apply<double>::value);
+  FATAL_EXPECT_EQ(2, try_index<ls>::apply<bool>::value);
+  FATAL_EXPECT_EQ(3, try_index<ls>::apply<void>::value);
+  FATAL_EXPECT_EQ(4, try_index<ls>::apply<float>::value);
+  FATAL_EXPECT_EQ(5, try_index<ls>::apply<char>::value);
+  FATAL_EXPECT_EQ(6, try_index<ls>::apply<unsigned>::value);
+  FATAL_EXPECT_EQ(size<ls>::value, try_index<ls>::apply<void *>::value);
+}
+
+FATAL_TEST(try_index apply, empty sequence) {
+  using sq = index_sequence<>;
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<0>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<8>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<9>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<6>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<7>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<5>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<4>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<1>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<3>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<2>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<10>::value);
+}
+
+FATAL_TEST(try_index apply, sequence) {
+  using sq = index_sequence<0, 8, 9, 6, 7, 5, 4, 1, 3, 2>;
+  FATAL_EXPECT_EQ(0, try_index<sq>::apply<0>::value);
+  FATAL_EXPECT_EQ(1, try_index<sq>::apply<8>::value);
+  FATAL_EXPECT_EQ(2, try_index<sq>::apply<9>::value);
+  FATAL_EXPECT_EQ(3, try_index<sq>::apply<6>::value);
+  FATAL_EXPECT_EQ(4, try_index<sq>::apply<7>::value);
+  FATAL_EXPECT_EQ(5, try_index<sq>::apply<5>::value);
+  FATAL_EXPECT_EQ(6, try_index<sq>::apply<4>::value);
+  FATAL_EXPECT_EQ(7, try_index<sq>::apply<1>::value);
+  FATAL_EXPECT_EQ(8, try_index<sq>::apply<3>::value);
+  FATAL_EXPECT_EQ(9, try_index<sq>::apply<2>::value);
+  FATAL_EXPECT_EQ(size<sq>::value, try_index<sq>::apply<10>::value);
+}
+
+} // namespace fatal {

commit 02cc24a864e5c9926b1c7a9e3e27f55d0ea55314

- properly failing compilation when passing invalid index to `index_of`
Summary:
(with `try_index_of` being the forgiving alternative)
  broken by e030ff227a001f7265fb75e2d6422847ff34eaac
Closes https://github.com/facebook/fatal/pull/104

Reviewed By: meyering

Differential Revision: D5722625

Pulled By: yfeldblum

fbshipit-source-id: 99c9c08dd9910b566df65b498a665f97cb8eb6bc

fatal/type/impl/slice.h
@@ -602,10 +602,10 @@ template <typename T, T Value, std::size_t Index>
 static std::integral_constant<std::size_t, Index> IV(vi<T, Value, Index>);
 
 template <std::size_t, typename T, T Value, std::size_t Index>
-static std::integral_constant<std::size_t, Index> IV(vi<T, Value, Index>);
+static std::integral_constant<std::size_t, Index> TIV(vi<T, Value, Index>);
 
 template <std::size_t Size, typename T, T>
-static std::integral_constant<std::size_t, Size> IV(...);
+static std::integral_constant<std::size_t, Size> TIV(...);
 
 template <typename...> struct i;
 
@@ -653,7 +653,7 @@ template <
 struct ti<Sequence<T, Values...>, index_sequence<Indexes...>> {
   template <T Value>
   using apply = decltype(
-    IV<sizeof...(Values), T, Value>(inherit<vi<T, Values, Indexes>...>())
+    TIV<sizeof...(Values), T, Value>(inherit<vi<T, Values, Indexes>...>())
   );
 };
 