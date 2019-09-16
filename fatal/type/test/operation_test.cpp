c6972d08567fea24cc92636234c09106ce144ebb
Yedidya Feldblum <yfeldblum@fb.com>
2019-09-16 15:13:04 -0700

    Cut deprecated constant_sequence
    Summary: [Fatal] Cut deprecated `constant_sequence`. Users may generally use `sequence` or `std::integer_sequence` instead.
    
    Reviewed By: Orvid
    
    Differential Revision: D17391844
    
    fbshipit-source-id: a583b8ca64fba30794e7bced3dd6c4ea57616b42

--- fatal/type/test/operation_test.cpp
+++ fatal/type/test/operation_test.cpp
@@ -13,9 +13,7 @@
 
 #include <fatal/preprocessor.h>
 #include <fatal/type/deprecated/type_map.h>
-#include <fatal/type/deprecated/constant_sequence.h>
 #include <fatal/type/sequence.h>
-#include <fatal/type/test/parse_sequence_input.h>
 
 #include <tuple>
 #include <type_traits>
@@ -26,7 +24,7 @@ template <typename> struct dummy_unit {};
 template <typename, typename> struct dummy_pair {};
 template <typename...> struct dummy_list {};
 template <typename T, T...> struct dummy_sequence {};
-template <int... Values> using int_seq = constant_sequence<int, Values...>;
+template <int... Values> using int_seq = sequence<int, Values...>;
 
 ////////////
 // expand //
@@ -214,26 +212,26 @@ FATAL_TEST(operation, flatten) {
 FATAL_TEST(operation, flatten_sequence) {
   FATAL_EXPECT_SAME<
     int_seq<>,
-    flatten_sequence<int, constant_sequence>::apply<>
+    flatten_sequence<int, sequence>::apply<>
   >();
 
   FATAL_EXPECT_SAME<
     int_seq<>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
       int_seq<>
     >
   >();
 
   FATAL_EXPECT_SAME<
     int_seq<10>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
       int_seq<10>
     >
   >();
 
   FATAL_EXPECT_SAME<
     int_seq<10>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
       int_seq<10>,
       int_seq<>
     >
@@ -241,7 +239,7 @@ FATAL_TEST(operation, flatten_sequence) {
 
   FATAL_EXPECT_SAME<
     int_seq<10>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
       int_seq<>,
       int_seq<10>
     >
@@ -249,7 +247,7 @@ FATAL_TEST(operation, flatten_sequence) {
 
   FATAL_EXPECT_SAME<
     int_seq<10>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
       int_seq<>,
       int_seq<10>,
       int_seq<>
@@ -258,7 +256,7 @@ FATAL_TEST(operation, flatten_sequence) {
 
   FATAL_EXPECT_SAME<
     int_seq<10, 20, 30, 40, 50>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
       int_seq<10>,
       int_seq<20>,
       int_seq<30>,
@@ -269,23 +267,7 @@ FATAL_TEST(operation, flatten_sequence) {
 
   FATAL_EXPECT_SAME<
     int_seq<10, 20, 30, 40, 50>,
-    flatten_sequence<int, constant_sequence>::apply<
-      int_seq<10>,
-      int_seq<>,
-      int_seq<20>,
-      int_seq<>,
-      int_seq<30>,
-      int_seq<>,
-      int_seq<40>,
-      int_seq<>,
-      int_seq<50>
-    >
-  >();
-
-  FATAL_EXPECT_SAME<
-    int_seq<10, 20, 30, 40, 50>,
-    flatten_sequence<int, constant_sequence>::apply<
-      int_seq<>,
+    flatten_sequence<int, sequence>::apply<
       int_seq<10>,
       int_seq<>,
       int_seq<20>,
@@ -300,7 +282,23 @@ FATAL_TEST(operation, flatten_sequence) {
 
   FATAL_EXPECT_SAME<
     int_seq<10, 20, 30, 40, 50>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
+      int_seq<>,
+      int_seq<10>,
+      int_seq<>,
+      int_seq<20>,
+      int_seq<>,
+      int_seq<30>,
+      int_seq<>,
+      int_seq<40>,
+      int_seq<>,
+      int_seq<50>
+    >
+  >();
+
+  FATAL_EXPECT_SAME<
+    int_seq<10, 20, 30, 40, 50>,
+    flatten_sequence<int, sequence>::apply<
       int_seq<10>,
       int_seq<>,
       int_seq<20>,
@@ -316,7 +314,7 @@ FATAL_TEST(operation, flatten_sequence) {
 
   FATAL_EXPECT_SAME<
     int_seq<10, 20, 30, 40, 50>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
       int_seq<>,
       int_seq<10>,
       int_seq<>,
@@ -333,7 +331,7 @@ FATAL_TEST(operation, flatten_sequence) {
 
   FATAL_EXPECT_SAME<
     int_seq<10, 20, 30, 40, 50, 60, 70>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
       int_seq<10>,
       int_seq<20, 30>,
       int_seq<40>,
@@ -343,21 +341,7 @@ FATAL_TEST(operation, flatten_sequence) {
 
   FATAL_EXPECT_SAME<
     int_seq<10, 20, 30, 40, 50, 60, 70>,
-    flatten_sequence<int, constant_sequence>::apply<
-      int_seq<10>,
-      int_seq<>,
-      int_seq<20, 30>,
-      int_seq<>,
-      int_seq<40>,
-      int_seq<>,
-      int_seq<50, 60, 70>
-    >
-  >();
-
-  FATAL_EXPECT_SAME<
-    int_seq<10, 20, 30, 40, 50, 60, 70>,
-    flatten_sequence<int, constant_sequence>::apply<
-      int_seq<>,
+    flatten_sequence<int, sequence>::apply<
       int_seq<10>,
       int_seq<>,
       int_seq<20, 30>,
@@ -370,7 +354,21 @@ FATAL_TEST(operation, flatten_sequence) {
 
   FATAL_EXPECT_SAME<
     int_seq<10, 20, 30, 40, 50, 60, 70>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
+      int_seq<>,
+      int_seq<10>,
+      int_seq<>,
+      int_seq<20, 30>,
+      int_seq<>,
+      int_seq<40>,
+      int_seq<>,
+      int_seq<50, 60, 70>
+    >
+  >();
+
+  FATAL_EXPECT_SAME<
+    int_seq<10, 20, 30, 40, 50, 60, 70>,
+    flatten_sequence<int, sequence>::apply<
       int_seq<10>,
       int_seq<>,
       int_seq<20, 30>,
@@ -384,7 +382,7 @@ FATAL_TEST(operation, flatten_sequence) {
 
   FATAL_EXPECT_SAME<
     int_seq<10, 20, 30, 40, 50, 60, 70>,
-    flatten_sequence<int, constant_sequence>::apply<
+    flatten_sequence<int, sequence>::apply<
       int_seq<>,
       int_seq<10>,
       int_seq<>,
@@ -409,7 +407,7 @@ template <int Value>
 struct tr: public std::integral_constant<int, Value> {};
 
 template <typename T>
-using to_seq = typename T::template apply_typed_values<int, constant_sequence>;
+using to_seq = typename T::template apply_typed_values<int, sequence>;
 
 template <typename T>
 using cut_last = typename T::template split<T::size - 1>::template transform<
@@ -510,21 +508,21 @@ FATAL_TEST(operation, expand_recursive_map) {
 
   FATAL_EXPECT_SAME<
     type_list<
-      constant_sequence<int, 0, 4, 3, 3, 0>,
-      constant_sequence<int, 0, 4, 3, 1, 1>,
-      constant_sequence<int, 0, 4, 2, 0, 2>,
-      constant_sequence<int, 0, 5, 1, 2, 3, 4, 5, 3>,
-      constant_sequence<int, 0, 5, 1, 2, 3, 4, 6, 4>,
-      constant_sequence<int, 0, 5, 1, 2, 3, 7, 8, 9, 5>,
-      constant_sequence<int, 0, 5, 1, 2, 0, 6>,
-      constant_sequence<int, 0, 5, 1, 1, 7>,
-      constant_sequence<int, 0, 6, 8>,
-      constant_sequence<int, 2, 7, 9>,
-      constant_sequence<int, 2, 2, 10>,
-      constant_sequence<int, 2, 0, 11>,
-      constant_sequence<int, 3, 9, 12>,
-      constant_sequence<int, 3, 8, 0, 13>,
-      constant_sequence<int, 3, 6, 14>
+      sequence<int, 0, 4, 3, 3, 0>,
+      sequence<int, 0, 4, 3, 1, 1>,
+      sequence<int, 0, 4, 2, 0, 2>,
+      sequence<int, 0, 5, 1, 2, 3, 4, 5, 3>,
+      sequence<int, 0, 5, 1, 2, 3, 4, 6, 4>,
+      sequence<int, 0, 5, 1, 2, 3, 7, 8, 9, 5>,
+      sequence<int, 0, 5, 1, 2, 0, 6>,
+      sequence<int, 0, 5, 1, 1, 7>,
+      sequence<int, 0, 6, 8>,
+      sequence<int, 2, 7, 9>,
+      sequence<int, 2, 2, 10>,
+      sequence<int, 2, 0, 11>,
+      sequence<int, 3, 9, 12>,
+      sequence<int, 3, 8, 0, 13>,
+      sequence<int, 3, 6, 14>
     >,
     expand_recursive_map<type_map, type_list>::apply<big_map>
       ::transform<to_seq>
@@ -532,106 +530,25 @@ FATAL_TEST(operation, expand_recursive_map) {
 
   FATAL_EXPECT_SAME<
     type_map<
-      type_pair<constant_sequence<int, 0, 4, 3, 3>, tr<0>>,
-      type_pair<constant_sequence<int, 0, 4, 3, 1>, tr<1>>,
-      type_pair<constant_sequence<int, 0, 4, 2, 0>, tr<2>>,
-      type_pair<constant_sequence<int, 0, 5, 1, 2, 3, 4, 5>, tr<3>>,
-      type_pair<constant_sequence<int, 0, 5, 1, 2, 3, 4, 6>, tr<4>>,
-      type_pair<constant_sequence<int, 0, 5, 1, 2, 3, 7, 8, 9>, tr<5>>,
-      type_pair<constant_sequence<int, 0, 5, 1, 2, 0>, tr<6>>,
-      type_pair<constant_sequence<int, 0, 5, 1, 1>, tr<7>>,
-      type_pair<constant_sequence<int, 0, 6>, tr<8>>,
-      type_pair<constant_sequence<int, 2, 7>, tr<9>>,
-      type_pair<constant_sequence<int, 2, 2>, tr<10>>,
-      type_pair<constant_sequence<int, 2, 0>, tr<11>>,
-      type_pair<constant_sequence<int, 3, 9>, tr<12>>,
-      type_pair<constant_sequence<int, 3, 8, 0>, tr<13>>,
-      type_pair<constant_sequence<int, 3, 6>, tr<14>>
+      type_pair<sequence<int, 0, 4, 3, 3>, tr<0>>,
+      type_pair<sequence<int, 0, 4, 3, 1>, tr<1>>,
+      type_pair<sequence<int, 0, 4, 2, 0>, tr<2>>,
+      type_pair<sequence<int, 0, 5, 1, 2, 3, 4, 5>, tr<3>>,
+      type_pair<sequence<int, 0, 5, 1, 2, 3, 4, 6>, tr<4>>,
+      type_pair<sequence<int, 0, 5, 1, 2, 3, 7, 8, 9>, tr<5>>,
+      type_pair<sequence<int, 0, 5, 1, 2, 0>, tr<6>>,
+      type_pair<sequence<int, 0, 5, 1, 1>, tr<7>>,
+      type_pair<sequence<int, 0, 6>, tr<8>>,
+      type_pair<sequence<int, 2, 7>, tr<9>>,
+      type_pair<sequence<int, 2, 2>, tr<10>>,
+      type_pair<sequence<int, 2, 0>, tr<11>>,
+      type_pair<sequence<int, 3, 9>, tr<12>>,
+      type_pair<sequence<int, 3, 8, 0>, tr<13>>,
+      type_pair<sequence<int, 3, 6>, tr<14>>
     >,
     expand_recursive_map<type_map, type_list>::apply<big_map>
       ::transform<cut_last>::apply<type_map>
   >();
-}
-
-/////////////////
-// to_sequence //
-/////////////////
-
-FATAL_TEST(operation, to_sequence) {
-# define TEST_IMPL(T, Value, TChar, ...) \
-  do { \
-    FATAL_EXPECT_SAME< \
-      dummy_sequence<TChar, __VA_ARGS__>, \
-      to_sequence<dummy_sequence, TChar>::apply<T, Value> \
-    >(); \
-    FATAL_EXPECT_SAME< \
-      dummy_sequence<TChar, __VA_ARGS__>, \
-      to_sequence<dummy_sequence, TChar>::bind<T>::apply<Value> \
-    >(); \
-  } while (false)
-
-  FATAL_IMPL_PARSE_SEQUENCE_TEST_CALLS(TEST_IMPL);
-
-# undef TEST_IMPL
-}
-
-////////////////////
-// parse_sequence //
-////////////////////
-
-FATAL_TEST(operation, parse_sequence) {
-# define TEST_IMPL(T, Value, TChar, ...) \
-  do { \
-    using expected = std::integral_constant<T, Value>; \
-    \
-    FATAL_EXPECT_SAME< \
-      expected, \
-      parse_sequence<T>::bind<TChar>::apply<__VA_ARGS__> \
-    >(); \
-    \
-    FATAL_EXPECT_SAME< \
-      expected, \
-      parse_sequence<T>::apply<TChar, __VA_ARGS__> \
-    >(); \
-    \
-    FATAL_EXPECT_SAME< \
-      expected, \
-      parse_sequence<T>::from<dummy_sequence<TChar, __VA_ARGS__>> \
-    >(); \
-  } while (false)
-
-  FATAL_IMPL_PARSE_SEQUENCE_TEST_CALLS(TEST_IMPL);
-
-# undef TEST_IMPL
-}
-
-/////////////////////
-// composite tests //
-/////////////////////
-
-FATAL_STR(joined1, "1:2:3:4:5");
-FATAL_STR(joined2, "100");
-FATAL_STR(joined3, "1:22:333:4444:55555");
-
-FATAL_TEST(constant_sequence, int seq to joined string) {
-# define TEST_IMPL(Expected, ...) \
-    do { \
-      using expected = Expected; \
-      using input = int_seq<__VA_ARGS__>; \
-      using actual = input::typed_list_transform< \
-        to_constant_sequence<>::apply \
-      >::interleave<constant_sequence<char, ':'>>::apply< \
-        flatten_sequence<char, constant_sequence>::apply \
-      >; \
-      FATAL_EXPECT_SAME<expected, actual>(); \
-    } while (false)
-
-  TEST_IMPL(constant_sequence<char>);
-  TEST_IMPL(joined1, 1, 2, 3, 4, 5);
-  TEST_IMPL(joined2, 100);
-  TEST_IMPL(joined3, 1, 22, 333, 4444, 55555);
-
-# undef TEST_IMPL
 }
 
 } // namespace fatal {
