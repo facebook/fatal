2602114b4814b5cc6693456521cc880e8c427461
Yedidya Feldblum <yfeldblum@meta.com>
2024-02-26 12:51:11 -0800

    let the driver in fatal/lesson/ be a source file
    Reviewed By: Orvid, dmm-fb
    
    Differential Revision: D54123652
    
    fbshipit-source-id: 768e18abf550eed02fa6f97fb83e2b75d30060c7

--- fatal/lesson/driver.cpp
+++ fatal/lesson/driver.cpp
@@ -7,26 +7,9 @@
  *  of patent rights can be found in the PATENTS file in the same directory.
  */
 
-#ifndef FATAL_INCLUDE_fatal_lesson_driver_h
-#define FATAL_INCLUDE_fatal_lesson_driver_h
-
 #include <fatal/lesson/lesson.h>
-#include <fatal/test/string.h>
-#include <fatal/test/type.h>
-
-#include <algorithm>
-#include <array>
-#include <functional>
-#include <string>
-#include <tuple>
-#include <type_traits>
-#include <unordered_map>
-#include <utility>
-#include <vector>
 
 int main() {
   fatal::lesson::registry::run();
   return 0;
 }
-
-#endif // FATAL_INCLUDE_fatal_lesson_driver_h
