4d55c8e6e0b23fa469293620a76c0238bd9b7491
Yedidya Feldblum <yfeldblum@meta.com>
2025-09-24 14:22:08 -0700

    let args_test use std::array vs C array
    Reviewed By: dmm-fb
    
    Differential Revision: D83173252
    
    fbshipit-source-id: d280f2716a49d96e4494c39b6ee1bc143d903371

--- fatal/test/test/args_test.cpp
+++ fatal/test/test/args_test.cpp
@@ -17,46 +17,46 @@
 using Args = std::map<std::string, std::string>;
 
 FATAL_TEST(args, parse_args_empty) {
-  char const * const argv[] = {"prog"};
-  int const argc = sizeof(argv) / sizeof(*argv);
+  constexpr std::array argv = {"prog"};
+  int const argc = argv.size();
   FATAL_EXPECT_EQ(1, argc); // sanity
 
-  auto const args = fatal::test_impl::args::parse_args<Args>(argc, argv);
+  auto const args = fatal::test_impl::args::parse_args<Args>(argc, argv.data());
   FATAL_EXPECT_EQ(Args({}), args);
 }
 
 FATAL_TEST(args, parse_args_one_basic) {
-  char const * const argv[] = {"prog", "x"};
-  int const argc = sizeof(argv) / sizeof(*argv);
+  constexpr std::array argv = {"prog", "x"};
+  int const argc = argv.size();
   FATAL_EXPECT_EQ(2, argc); // sanity
 
-  auto const args = fatal::test_impl::args::parse_args<Args>(argc, argv);
+  auto const args = fatal::test_impl::args::parse_args<Args>(argc, argv.data());
   FATAL_EXPECT_EQ(Args({{"x", ""}}), args);
 }
 
 FATAL_TEST(args, parse_args_one_key) {
-  char const * const argv[] = {"prog", "y="};
-  int const argc = sizeof(argv) / sizeof(*argv);
+  constexpr std::array argv = {"prog", "y="};
+  int const argc = argv.size();
   FATAL_EXPECT_EQ(2, argc); // sanity
 
-  auto const args = fatal::test_impl::args::parse_args<Args>(argc, argv);
+  auto const args = fatal::test_impl::args::parse_args<Args>(argc, argv.data());
   FATAL_EXPECT_EQ(Args({{"y", ""}}), args);
 }
 
 FATAL_TEST(args, parse_args_one_key_value) {
-  char const * const argv[] = {"prog", "z=a"};
-  int const argc = sizeof(argv) / sizeof(*argv);
+  constexpr std::array argv = {"prog", "z=a"};
+  int const argc = argv.size();
   FATAL_EXPECT_EQ(2, argc); // sanity
 
-  auto const args = fatal::test_impl::args::parse_args<Args>(argc, argv);
+  auto const args = fatal::test_impl::args::parse_args<Args>(argc, argv.data());
   FATAL_EXPECT_EQ(Args({{"z", "a"}}), args);
 }
 
 FATAL_TEST(args, parse_args_multi) {
-  char const * const argv[] = {"prog", "x", "y=", "z=a"};
-  int const argc = sizeof(argv) / sizeof(*argv);
+  constexpr std::array argv = {"prog", "x", "y=", "z=a"};
+  int const argc = argv.size();
   FATAL_EXPECT_EQ(4, argc); // sanity
 
-  auto const args = fatal::test_impl::args::parse_args<Args>(argc, argv);
+  auto const args = fatal::test_impl::args::parse_args<Args>(argc, argv.data());
   FATAL_EXPECT_EQ(Args({{"x", ""}, {"y", ""}, {"z", "a"}}), args);
 }
